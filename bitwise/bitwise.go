83ac3569de5cecaecf262c88c506f066c006b0b3
Davor Kapsa <davorkapsa@gmail.com>
2016-05-13 12:55:14 +0200

    bitwise: add bin

--- bitwise/bitwise.go
+++ bitwise/bitwise.go
@@ -1,16 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
 
 func main() {
 	bitwiseAnd()
 }
 
 func bitwiseAnd() {
-	a := byte(25)
-	b := byte(37)
+	fmt.Println("\nbitwiseAND")
+	a := bin("101")
+	b := bin("011")
 
-	// bit is true only if both source(a,b) bit are 1(true).
+	// bit is 1 only if
+	// sources(a,b) bit are 1.
 	c := a & b
 
 	prt("a", a)
@@ -18,6 +24,15 @@ func bitwiseAnd() {
 	prt("a & b", c)
 }
 
-func prt(s string, b byte) {
-	fmt.Printf("%-10s: %08b\n", s, b)
+// bin converts binary string to integer.
+func bin(s string) int64 {
+	i, err := strconv.ParseInt(s, 2, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return i
+}
+
+func prt(s string, i int64) {
+	fmt.Printf("%-10s: %8b = %d\n", s, i, i)
 }
