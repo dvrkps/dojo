2b84d936802d86a8820911254fa45d5685d87a67
Davor Kapsa <davorkapsa@gmail.com>
2016-05-14 18:53:17 +0200

    bitwise: add operators

--- bitwise/bitwise.go
+++ bitwise/bitwise.go
@@ -7,9 +7,11 @@ import (
 )
 
 func main() {
-	bitwiseAND()
+	// bitwiseAND()
 
-	bitwiseOR()
+	// bitwiseOR()
+
+	operators()
 }
 
 func bitwiseAND() {
@@ -49,4 +51,34 @@ func bin(s string) int64 {
 
 func prt(s string, i int64) {
 	fmt.Printf("%-10s: %8b = %d\n", s, i, i)
+}
+
+func operators() {
+	pf := func(s string, vals ...int) {
+		const (
+			row = "%04b = %d\n"
+		)
+
+		fmt.Println(s)
+		for _, i := range vals {
+			fmt.Printf(row, i, i)
+		}
+		fmt.Println()
+	}
+
+	// Use bitwise AND & to get the bits
+	// that are in 3 AND 6
+	pf("and", 3, 6, 3&6)
+
+	// Use bitwise OR | to get the bits
+	// that are in 1 OR 4
+	pf("or", 3, 6, 3|6)
+
+	// Use bitwise XOR ^ to get the bits
+	// that are in 3 OR 6 BUT NOT BOTH
+	pf("xor", 3, 6, 3^6)
+
+	// Use bit clear AND NOT &^ to get the bits
+	// that are in 3 AND NOT 6 (order matters)
+	pf("and not", 3, 6, 3&^6)
 }
