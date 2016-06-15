4d2dfc6220eb9c131509837bcdf3a68c9e16f66a
Davor Kapsa <davorkapsa@gmail.com>
2016-06-15 16:44:46 +0200

    iterbench: add subtests

--- iterbench/iterbench_test.go
+++ iterbench/iterbench_test.go
@@ -2,8 +2,37 @@ package iterbench
 
 import "testing"
 
-func TestIterFor(t *testing.T) {
-	_ = iterFor(10)
+type testType struct {
+	in   int
+	want int
+}
+
+type testFunc func(int) int
+
+var tests = []testType{
+	{10, 55},
+	{100, 5050},
+	{1000, 500500},
+	{10000, 50005000},
+	{100000, 5000050000},
+}
+
+func testIter(t *testing.T, tests []testType, funcName string, fn testFunc) {
+	for _, tt := range tests {
+		if got := iterFor(tt.in); got != tt.want {
+			t.Errorf("%s(%d) = %d; want %d",
+				funcName, tt.in, got, tt.want)
+		}
+	}
+}
+
+func TestIter(t *testing.T) {
+	t.Run("iterFor", func(t *testing.T) {
+		testIter(t, tests, "iterFor", iterFor)
+	})
+	t.Run("iterGoto", func(t *testing.T) {
+		testIter(t, tests, "iterGoto", iterGoto)
+	})
 }
 
 func TestIterGoto(t *testing.T) {
