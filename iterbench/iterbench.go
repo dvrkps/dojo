1876badcee8bd79b2f9b400a8fe80170bc7cadff
Davor Kapsa <davorkapsa@gmail.com>
2016-06-15 13:29:01 +0200

    iterbench: add iterGoto

--- iterbench/iterbench.go
+++ iterbench/iterbench.go
@@ -16,6 +16,21 @@ func iterFor(lmt int) int {
 	return sum
 }
 
+func iterGoto(lmt int) int {
+	var (
+		i   int
+		sum int
+		ok  bool
+	)
+again:
+	i, ok = do(i, lmt)
+	sum += i
+	if !ok {
+		return sum
+	}
+	goto again
+}
+
 func do(i int, lmt int) (int, bool) {
 	i++
 	ok := i != lmt
--- iterbench/iterbench_test.go
+++ iterbench/iterbench_test.go
@@ -6,12 +6,24 @@ func TestIterFor(t *testing.T) {
 	_ = iterFor(10)
 }
 
+func TestIterGoto(t *testing.T) {
+	_ = iterGoto(10)
+}
+
 var result int
 
 func BenchmarkIterFor(b *testing.B) {
 	var r int
 	for n := 0; n < b.N; n++ {
 		r = iterFor(10)
+	}
+	result = r
+}
+
+func BenchmarkIterGoto(b *testing.B) {
+	var r int
+	for n := 0; n < b.N; n++ {
+		r = iterGoto(10)
 	}
 	result = r
 }
