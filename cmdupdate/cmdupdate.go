ba2f286ab4853674e140d662cfaf1ee02f902aa9
Davor Kapsa <davorkapsa@gmail.com>
2016-06-22 18:09:08 +0200

    cmdupdate: add update func

--- cmdupdate/cmdupdate.go
+++ cmdupdate/cmdupdate.go
@@ -1,10 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+)
 
 const version = "0.1.0"
 
 func main() {
+	up := flag.Bool("update", true, "update command")
+	flag.Parse()
+
 	fmt.Printf("version %s\n", version)
 
+	var code int
+	if *up {
+		code = update()
+	}
+
+	os.Exit(code)
+}
+
+const srcPath = "github.com/dvrkps/dojo/cmdupdate"
+
+func update() int {
+	cmd := exec.Command("go", "get", "-u", srcPath)
+	out, err := cmd.CombinedOutput()
+	var code int
+	if err != nil {
+		fmt.Println("ERROR")
+		code = 1
+	}
+	fmt.Println(string(out))
+	return code
 }
