ed8ec5eb1f59bea2294cce45708ebd4b837258d3
Davor Kapsa <davorkapsa@gmail.com>
2016-06-26 16:40:14 +0200

    file_scanner: move benchmarks

--- file_scanner/file_scanner_test.go
+++ file_scanner/file_scanner_test.go
@@ -42,25 +42,3 @@ func TestScanString(t *testing.T) {
 func TestScanBytes(t *testing.T) {
 	testScan(t, scanBytes)
 }
-
-var resultPersons Persons
-
-func BenchmarkScanString(b *testing.B) {
-	f := fakeReader(10000)
-	b.ResetTimer()
-	var r Persons
-	for n := 0; n < b.N; n++ {
-		r = scanString(f)
-	}
-	resultPersons = r
-}
-
-func BenchmarkScanBytes(b *testing.B) {
-	f := fakeReader(10000)
-	b.ResetTimer()
-	var r Persons
-	for n := 0; n < b.N; n++ {
-		r = scanBytes(f)
-	}
-	resultPersons = r
-}
