8a290d53eab201e7e3218662483dd55293184182
Davor Kapsa <davorkapsa@gmail.com>
2016-06-26 19:29:49 +0200

    file_scanner: clean concurrently tests

--- file_scanner/concurrently_test.go
+++ file_scanner/concurrently_test.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-	"testing"
-)
+import "testing"
 
 func TestScanConcurrently(t *testing.T) {
-	const noRows = 10
-	f := fakeReader(noRows)
-	ps := scanConcurrently(f)
-	fmt.Println(ps)
-	if got := len(ps); got != noRows {
-		t.Errorf("len(Persons) = %v; want %v", got, noRows)
-	}
-	want := Person{
-		ID:   4,
-		Name: "name4",
-		Age:  noRows - 4}
-	if got := ps[3]; !reflect.DeepEqual(got, want) {
-		t.Errorf("p[3] = %v; want %v", got, want)
-	}
+	testScan(t, scanConcurrently)
 }
