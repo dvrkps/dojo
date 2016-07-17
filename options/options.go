3e49bc9260a07206030561319458261ea40fa24d
Davor Kapsa <davorkapsa@gmail.com>
2016-07-17 10:41:41 +0200

    options: fix main

--- options/options.go
+++ options/options.go
@@ -7,9 +7,12 @@ import (
 )
 
 func main() {
-	srv := server.New(
+	srv, err := server.New(
 		server.Port(80),
 		server.Host("example.com"),
 		server.Port(82))
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Printf("%+v\n", srv)
 }
