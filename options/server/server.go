d1783c50ef05d31a57cafe45393c2905b1afacde
Davor Kapsa <davorkapsa@gmail.com>
2016-07-17 09:36:08 +0200

    options/server: add Option type

--- options/server/server.go
+++ options/server/server.go
@@ -20,8 +20,11 @@ func Port(port int) func(s *Server) {
 	}
 }
 
+// Option handle server option.
+type Option func(s *Server)
+
 // New creates new server.
-func New(options ...func(s *Server)) *Server {
+func New(options ...Option) *Server {
 	srv := &Server{}
 	for _, o := range options {
 		o(srv)
