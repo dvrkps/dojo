8bdf012205c85d44d48979fa6cf530cf2bf99fff
Davor Kapsa <davor.kapsa@gmail.com>
2016-09-16 22:36:10 +0200

    randtime: add randDurationInRange

--- randtime/randtime.go
+++ randtime/randtime.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
 
 func main() {
+	min := 1 * time.Nanosecond
+	max := 5 * time.Hour
+	rt := randDurationInRange(min, max)
+	fmt.Println(rt)
 }
 
-func randInRange(min, max int) int {
+func randDurationInRange(minDur, maxDur time.Duration) time.Duration {
+
+	min := int64(minDur)
+	max := int64(maxDur)
+
 	src := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(src)
-	return r.Intn(max-min) + min
+
+	x := r.Int63n(max-min) + min
+
+	return time.Duration(x)
 }
