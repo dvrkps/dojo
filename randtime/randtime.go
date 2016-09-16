73b807ae9d1d7f80b52e0f5c1681084450767f11
Davor Kapsa <davor.kapsa@gmail.com>
2016-09-16 21:54:19 +0200

    randtime: add randInRange func

--- randtime/randtime.go
+++ randtime/randtime.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
 func main() {
+}
+
+func randInRange(min, max int) int {
+	src := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(src)
+	return r.Intn(max-min) + min
 }
