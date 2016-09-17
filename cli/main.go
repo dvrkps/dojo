baf310ece7bb9f2003fe7013cf38c5617775022a
Davor Kapsa <davor.kapsa@gmail.com>
2016-09-17 11:27:54 +0200

    cli: add run func

--- cli/main.go
+++ cli/main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
 func main() {
+	os.Exit(run(1))
+}
+
+const (
+	exitOk = iota
+	exitErr
+)
+
+func run(in int) int {
+
+	if in < 1 {
+		log.Printf("%d < 1", in)
+		return exitErr
+	}
+
+	fmt.Printf("in = %d\n", in)
+
+	return exitOk
 }
--- cli/main_test.go
+++ cli/main_test.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"log"
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	log.SetFlags(0)
+
+	tests := map[string]struct {
+		in   int
+		want int
+	}{
+
+		"ok": {
+			in:   1,
+			want: 0},
+
+		"in < 1": {
+			in:   0,
+			want: 1},
+	}
+
+	for k, tt := range tests {
+		got := run(tt.in)
+		if got != tt.want {
+			t.Errorf("%s: run(%d) = %d; want %d",
+				k, tt.in, got, tt.want)
+		}
+
+	}
+
+}
