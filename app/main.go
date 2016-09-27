2ea1201794755816b38ff763ec51888122175877
Davor Kapsa <davor.kapsa@gmail.com>
2016-09-27 17:10:20 +0200

    app: add run func

--- app/main.go
+++ app/main.go
@@ -2,3 +2,7 @@ package main
 
 func main() {
 }
+
+func run() int {
+	return 0
+}
--- app/main_test.go
+++ app/main_test.go
@@ -1,1 +1,10 @@
 package main
+
+import "testing"
+
+func TestRun(t *testing.T) {
+	got := run()
+	if got != 0 {
+		t.Error("boom")
+	}
+}
