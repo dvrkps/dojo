7519c151c0fdcb007fc39b1e2265ad62e75d8d73
Davor Kapsa <davor.kapsa@gmail.com>
2016-09-29 13:19:57 +0200

    app: remove App.writeln

--- app/main.go
+++ app/main.go
@@ -70,13 +70,6 @@ func (a *App) output(typ int, format string, v ...interface{}) {
 	}
 }
 
-func (a *App) writeln(w io.Writer, v ...interface{}) {
-	_, err := fmt.Fprintln(w, v...)
-	if err != nil {
-		a.logger.Print(err)
-	}
-}
-
 // Printf prints to stdout like fmt.Printf.
 func (a *App) Printf(format string, v ...interface{}) {
 	a.output(fnPrintf, format, v...)
