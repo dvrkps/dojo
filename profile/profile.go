bdf6ba2625ba310de304bbce245ecc8c00128162
Davor Kapsa <davor.kapsa@gmail.com>
2016-10-07 12:16:34 +0200

    profile: add strData1

--- profile/profile.go
+++ profile/profile.go
@@ -1,1 +1,27 @@
 package profile
+
+import (
+	"io"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const maxValue = 99
+
+func strData1(n int) io.Reader {
+	vs := []string{}
+	for i := 0; i < n; i++ {
+		v := randValue(maxValue)
+		vs = append(vs, strconv.Itoa(v))
+	}
+	all := strings.Join(vs, "\n")
+	return strings.NewReader(all)
+}
+
+func randValue(max int) int {
+	src := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(src)
+	return r.Intn(max)
+}
--- profile/profile_test.go
+++ profile/profile_test.go
@@ -1,1 +1,24 @@
 package profile
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestStrData1(t *testing.T) {
+	_ = strData1(10)
+}
+
+func BenchmarkStrData1(b *testing.B) {
+	var name string
+	tests := []int{100, 1000, 10000, 100000}
+	for _, tt := range tests {
+		name = fmt.Sprintf("strData1(%d)", tt)
+
+		b.Run(name, func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				_ = strData1(tt)
+			}
+		})
+	}
+}
