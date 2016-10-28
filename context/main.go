f58e10e57724b2031780e0accc9ff8d1bea84472
Davor Kapsa <davor.kapsa@gmail.com>
2016-10-28 17:28:50 +0200

    context: add main and gen func

--- context/main.go
+++ context/main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for n := range gen(ctx) {
+		fmt.Println(n)
+		if n == 5 {
+			cancel()
+			break
+		}
+	}
+}
+
+func gen(ctx context.Context) <-chan int {
+
+	ch := make(chan int)
+
+	go func() {
+
+		var n int
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case ch <- n:
+				n++
+			}
+		}
+	}()
+	return ch
+}
