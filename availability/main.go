c518d53e2f80cbf3a4bb5bb2186914c160e70173
Davor Kapsa <davor.kapsa@gmail.com>
2016-11-02 22:03:47 +0100

    availability: add yearNs

--- availability/main.go
+++ availability/main.go
@@ -1,10 +1,15 @@
 package availability
 
+import "time"
+
 // time based formula
 // availability = yearUptime / ( yearUptime + yearDowntime )
 
 // aggregate availability formula
 // availability = daySuccessfulRequsts / dayTotalRequests
+
+// Year in nanoseconds.
+const yearNs = 3.154e16 * time.Nanosecond
 
 // Availability types.
 const (
