eca25d1d883f1013c8a027d440177dd5727bcfa3
Davor Kapsa <davor.kapsa@gmail.com>
2016-11-03 10:43:26 +0100

    nilinterface: add ex1

--- nilinterface/main.go
+++ nilinterface/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+func main() {
+	ex1()
+
+}
+
+func ex1() {
+	var i interface{} = nil
+	println("nil interface is nil?:", i == nil) // true
+	println(i)                                  // (0x0, 0x0)
+
+}
