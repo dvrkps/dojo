2473d464b2d0433463c8f68d80cf2b8dd2ec1fd0
Davor Kapsa <davor.kapsa@gmail.com>
2016-11-03 10:48:34 +0100

    nilinterface: add ex2

--- nilinterface/main.go
+++ nilinterface/main.go
@@ -1,13 +1,28 @@
 package main
 
 func main() {
+
 	ex1()
+
+	ex2()
 
 }
 
 func ex1() {
 	var i interface{} = nil
+	println("ex1")
 	println("nil interface is nil?:", i == nil) // true
 	println(i)                                  // (0x0, 0x0)
+	println()
 
+}
+
+func ex2() {
+	var v int = 10
+	var i interface{} = v
+	println("ex2")
+	println("interface-to-value is nil?:", i == nil) // false
+	println("value address:", &v)
+	println(i)
+	println()
 }
