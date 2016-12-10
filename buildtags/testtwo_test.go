e3364d0a65f00290913270c77dd8e2ae4f2c8f23
Davor Kapsa <davor.kapsa@gmail.com>
2016-12-10 20:51:57 +0100

    buildtags: clean testtwo

--- buildtags/testtwo_test.go
+++ buildtags/testtwo_test.go
@@ -5,23 +5,6 @@ package main
 import "testing"
 
 func TestTwoAdd(t *testing.T) {
-	tests := []struct {
-		in   int
-		want int
-	}{
-		{in: 21, want: 21},
-		{in: 22, want: 22},
-		{in: 23, want: 23},
-	}
-	for _, tt := range tests {
-		got := add(tt.in)
-		if got != tt.want {
-			t.Errorf("(%d) add(%d) = %d; want %d",
-				value,
-				tt.in,
-				got,
-				tt.want)
-		}
-	}
-
+	tests := []int{21, 22, 23}
+	testAdd(t, value, tests)
 }
