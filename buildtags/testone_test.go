f2d14a0fe94bb9233ab30552a3120dd47ccc584c
Davor Kapsa <davor.kapsa@gmail.com>
2016-12-10 20:51:43 +0100

    buildtags: clean testone

--- buildtags/testone_test.go
+++ buildtags/testone_test.go
@@ -5,23 +5,6 @@ package main
 import "testing"
 
 func TestOneAdd(t *testing.T) {
-	tests := []struct {
-		in   int
-		want int
-	}{
-		{in: 11, want: 11},
-		{in: 12, want: 12},
-		{in: 13, want: 13},
-	}
-	for _, tt := range tests {
-		got := add(tt.in)
-		if got != tt.want {
-			t.Errorf("(%d) add(%d) = %d; want %d",
-				value,
-				tt.in,
-				got,
-				tt.want)
-		}
-	}
-
+	tests := []int{11, 12, 13}
+	testAdd(t, value, tests)
 }
