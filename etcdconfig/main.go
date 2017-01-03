049663facb82a186135fbb66050bfd78f9aba847
Davor Kapsa <davor.kapsa@gmail.com>
2017-01-03 17:24:17 +0100

    etcdconfig: add raw etcd get

--- etcdconfig/main.go
+++ etcdconfig/main.go
@@ -1,4 +1,45 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+)
+
 func main() {
+	endpoints := []string{
+		":2379",
+	}
+	dialTimeout := 5 * time.Second
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   endpoints,
+		DialTimeout: dialTimeout,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cli.Close()
+
+	/*
+		_, err = cli.Put(context.TODO(), "foo", "bar")
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
+
+	requestTimeout := 5 * time.Second
+
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+	resp, err := cli.Get(ctx, "fo", clientv3.WithPrefix())
+	cancel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%#v\n\n", resp)
+	for _, ev := range resp.Kvs {
+		fmt.Printf("%#v : %#v \n", ev.Key, ev.Value)
+	}
 }
