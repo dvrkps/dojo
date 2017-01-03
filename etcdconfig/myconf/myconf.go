7deeb9e426538d218e639fdd890ac49d80fc7653
Davor Kapsa <davor.kapsa@gmail.com>
2017-01-03 17:48:44 +0100

    etcdconfig/myconf: add Config, Client and New

--- etcdconfig/myconf/myconf.go
+++ etcdconfig/myconf/myconf.go
@@ -1,1 +1,38 @@
 package myconf
+
+import (
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+)
+
+// Config holds configuration data.
+type Config struct {
+	Endpoints   []string
+	DialTimeout time.Duration
+}
+
+// Client is configuration client.
+type Client struct {
+	cli *clientv3.Client
+}
+
+// New creates configuration client.
+func New(cfg Config) (*Client, error) {
+
+	ec, err := clientv3.New(clientv3.Config{
+		Endpoints:   cfg.Endpoints,
+		DialTimeout: cfg.DialTimeout,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Client{
+		cli: ec,
+	}
+
+	return c, nil
+
+}
