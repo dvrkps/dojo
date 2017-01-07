cc012c31e130ab88782f9a246a2091bbafb97a2f
Davor Kapsa <davor.kapsa@gmail.com>
2017-01-07 20:37:35 +0100

    etcdconfig/myconfig: rename newData

--- etcdconfig/myconfig/data.go
+++ etcdconfig/myconfig/data.go
@@ -14,7 +14,7 @@ type data struct {
 	dm dataMap
 }
 
-func newStorage() *data {
+func newData() *data {
 	d := &data{}
 	d.av.Store(d.dm)
 
@@ -31,10 +31,10 @@ func (d *data) update(dm dataMap) {
 	d.mu.Lock()
 	defer d.mu.Unlock()
 
-	fresh := make(Map)
+	fresh := make(dataMap)
 
 	// clone old values
-	old := d.av.Load().(Map)
+	old := d.av.Load().(dataMap)
 	for k, v := range old {
 		fresh[k] = v
 	}
@@ -46,29 +46,3 @@ func (d *data) update(dm dataMap) {
 
 	d.av.Store(fresh)
 }
-
-/*
-type Map map[string]string
-    var m Value
-    m.Store(make(Map))
-    var mu sync.Mutex // used only by writers
-    // read function can be used to read the data without further synchronization
-    read := func(key string) (val string) {
-            m1 := m.Load().(Map)
-            return m1[key]
-    }
-    // insert function can be used to update the data without further synchronization
-    insert := func(key, val string) {
-            mu.Lock() // synchronize with other potential writers
-            defer mu.Unlock()
-            m1 := m.Load().(Map) // load current value of the data structure
-            m2 := make(Map)      // create a new value
-            for k, v := range m1 {
-                    m2[k] = v // copy all data from the current object to the new one
-            }
-            m2[key] = val // do the update that we need
-            m.Store(m2)   // atomically replace the current object with the new one
-            // At this point all new readers start working with the new version.
-            // The old version will be garbage collected once the existing readers
-            // (if any) are done with it.
-    }*/
