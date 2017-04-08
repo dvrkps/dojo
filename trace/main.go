442010e02c6c07835552d172593d2f60a095725f
Davor Kapsa <davor.kapsa@gmail.com>
2017-04-08 19:27:59 +0200

    trace: add setupTrace, gen and sq func

--- trace/main.go
+++ trace/main.go
@@ -1,3 +1,72 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime/trace"
+)
+
+func setupTrace(path string) func() {
+
+	f, err := os.Create(path)
+	if err != nil {
+		log.Print(err)
+		return func() {}
+	}
+
+	err = trace.Start(f)
+	if err != nil {
+		log.Println(err)
+		err = f.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	return func() {
+
+		trace.Stop()
+
+		if f != nil {
+			err = f.Close()
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+
+}
+
+func main() {
+	stop := setupTrace("trace.out")
+	defer stop()
+
+	out := sq(sq(gen(2, 3)))
+
+	for n := range out {
+		fmt.Println(n)
+	}
+}
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
