5b9d58a34e75cbaaf9d1db161cec4d446a5c8645
Davor Kapsa <davor.kapsa@gmail.com>
2017-04-17 14:14:27 +0200

    nilrange: add one func

--- nilrange/main.go
+++ nilrange/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+func main() {
+	sum := one([]int{1, 2, 3})
+	println(sum)
+}
+
+func one(data []int) int {
+	var sum int
+	for _, i := range data {
+		sum += i
+	}
+	return sum
+}
