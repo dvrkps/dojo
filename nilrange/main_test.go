028dda01d4a2b175b81c87afc0a3f2433e0d484c
Davor Kapsa <davor.kapsa@gmail.com>
2017-04-17 14:41:00 +0200

    nilrange: move benchmarks

--- nilrange/main_test.go
+++ nilrange/main_test.go
@@ -1,37 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func Test(t *testing.T) {}
-
-var result int
-
-func benchmarkHelper(b *testing.B, fn func([]int) int, args []int) {
-	var r int
-	b.ResetTimer()
-	for n := 0; n < b.N; n++ {
-		r = fn(args)
-	}
-	result = r
-}
-
-var benchCases = []struct {
-	name string
-	fn   func([]int) int
-	args []int
-}{
-	{"one", one, nil},
-	{"one", one, []int{1, 2, 3}},
-}
-
-func BenchmarkAll(b *testing.B) {
-	for _, bc := range benchCases {
-		name := fmt.Sprintf("%s(%v)", bc.name, bc.args)
-		b.Run(name, func(b *testing.B) {
-			benchmarkHelper(b, bc.fn, bc.args)
-		})
-	}
-}
