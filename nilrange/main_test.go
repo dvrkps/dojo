f79e1f918768e717ad2f707d29164b7d404e4aef
Davor Kapsa <davor.kapsa@gmail.com>
2017-04-17 16:01:06 +0200

    nilrange: add TestAll and testCases

--- nilrange/main_test.go
+++ nilrange/main_test.go
@@ -4,4 +4,23 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) {}
+var testCases = []struct {
+	name string
+	fn   func([]int) int
+	args []int
+	want int
+}{
+	{"one(nil)", one, nil, 0},
+	{"one(1,2,3)", one, []int{1, 2, 3}, 6},
+}
+
+func TestAll(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.fn(tc.args)
+			if got != tc.want {
+				t.Fatalf("got %v; want %v", got, tc.want)
+			}
+		})
+	}
+}
