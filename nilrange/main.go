bc181573d4fac3b566c34612109ca590b3a1c3b0
Davor Kapsa <davor.kapsa@gmail.com>
2017-04-17 16:40:28 +0200

    nilrange: add run func

--- nilrange/main.go
+++ nilrange/main.go
@@ -6,6 +6,22 @@ func main() {
 	os.Exit(run())
 }
 
+func run() int {
+	all := []int{
+		one(nil),
+		one([]int{1, 2, 3}),
+		two(nil),
+		two([]int{1, 2, 3}),
+		three(nil),
+		three([]int{1, 2, 3}),
+	}
+	var sum int
+	for _, i := range all {
+		sum += i
+	}
+	return 0
+}
+
 func one(data []int) int {
 	var sum int
 	for _, i := range data {
