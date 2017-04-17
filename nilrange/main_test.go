335c945eae5eefe0cbe6f6a759b90afb84b4a8db
Davor Kapsa <davor.kapsa@gmail.com>
2017-04-17 16:18:56 +0200

    nilrange: add two tests

--- nilrange/main_test.go
+++ nilrange/main_test.go
@@ -12,6 +12,8 @@ var testCases = []struct {
 }{
 	{"one(nil)", one, nil, 0},
 	{"one(1,2,3)", one, []int{1, 2, 3}, 6},
+	{"two(nil)", two, nil, 0},
+	{"two(1,2,3)", two, []int{1, 2, 3}, 6},
 }
 
 func TestAll(t *testing.T) {
