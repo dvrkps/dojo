055aa4b29ac97981736094a9ac7222354a160f0e
Davor Kapsa <davor.kapsa@gmail.com>
2017-04-19 20:01:14 +0200

    sum: add basic

--- sum/sum.go
+++ sum/sum.go
@@ -1,1 +1,13 @@
 package sum
+
+func basic(sum int, nums ...int) bool {
+	for _, x := range nums {
+		for _, y := range nums {
+			v := x + y
+			if sum == v {
+				return true
+			}
+		}
+	}
+	return false
+}
