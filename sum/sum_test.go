c6c7c914846a0f08636d980e4c889be77607607e
Davor Kapsa <davor.kapsa@gmail.com>
2017-04-19 20:01:45 +0200

    sum: add basic tests

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,1 +1,31 @@
 package sum
+
+import "testing"
+
+var testCases = []struct {
+	name string
+	fn   func(int, ...int) bool
+	sum  int
+	in   []int
+	want bool
+}{
+	{name: "basic(8,1,2,3,9)",
+		fn:   basic,
+		sum:  8,
+		in:   []int{1, 2, 3, 9},
+		want: false},
+	{name: "basic(8,1,2,4,4)",
+		fn:   basic,
+		sum:  8,
+		in:   []int{1, 2, 4, 4},
+		want: true},
+}
+
+func TestAll(t *testing.T) {
+	for _, tc := range testCases {
+		got := tc.fn(tc.sum, tc.in...)
+		if got != tc.want {
+			t.Errorf("%s = %v; want %v", tc.name, got, tc.want)
+		}
+	}
+}
