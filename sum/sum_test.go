2a6f2bdece786ec13022b730c23b6744dce79a43
Davor Kapsa <davor.kapsa@gmail.com>
2017-04-19 22:45:52 +0200

    sum: add optim tests

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -49,6 +49,16 @@ var testCases = []struct {
 		sum:  8,
 		in:   []int{1, 2, 4, 4},
 		want: true},
+	{name: "optim(8,1,2,3,9)",
+		fn:   optim,
+		sum:  8,
+		in:   []int{1, 2, 3, 9},
+		want: false},
+	{name: "optim(8,1,2,4,4)",
+		fn:   optim,
+		sum:  8,
+		in:   []int{1, 2, 4, 4},
+		want: true},
 }
 
 func TestAll(t *testing.T) {
