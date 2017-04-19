e36be44aa081f8816ef8fb3335d965dda61d4ab5
Davor Kapsa <davor.kapsa@gmail.com>
2017-04-19 20:34:54 +0200

    sum: add better

--- sum/sum.go
+++ sum/sum.go
@@ -11,3 +11,15 @@ func basic(sum int, nums ...int) bool {
 	}
 	return false
 }
+
+func better(sum int, nums ...int) bool {
+	for _, x := range nums {
+		for _, y := range nums {
+			v := x + y
+			if sum == v {
+				return true
+			}
+		}
+	}
+	return false
+}
