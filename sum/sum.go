d7e6d97e7d978099c7460efa9d4baa7eb8ccaed7
Davor Kapsa <davor.kapsa@gmail.com>
2017-04-19 20:45:54 +0200

    sum: fix better

--- sum/sum.go
+++ sum/sum.go
@@ -13,10 +13,14 @@ func basic(sum int, nums ...int) bool {
 }
 
 func better(sum int, nums ...int) bool {
-	for _, x := range nums {
-		for _, y := range nums {
-			v := x + y
-			if sum == v {
+	max := len(nums)
+	var x, y, s int
+	for ix := 0; ix < max; ix++ {
+		x = nums[ix]
+		for iy := ix + 1; iy < max; iy++ {
+			y = nums[iy]
+			s = x + y
+			if sum == s {
 				return true
 			}
 		}
