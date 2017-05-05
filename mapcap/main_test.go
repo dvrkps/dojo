66187aecc9c61649d9d11460aa15b4eb2ffbf48d
Davor Kapsa <davor.kapsa@gmail.com>
2017-05-05 19:41:44 +0200

    mapcap: clean benchmarks

--- mapcap/main_test.go
+++ mapcap/main_test.go
@@ -17,15 +17,25 @@ func TestInitMap(t *testing.T) {
 
 var result map[int]int
 
-var benchs = []int{100} //, 1000, 10000}
+var benchs = []int{30000, 100000} //, 1000, 10000}
 
 func BenchmarkMaps(b *testing.B) {
 	var name string
 	for _, bb := range benchs {
 
-		name = fmt.Sprintf("baseMap(%d)", bb)
+		name = fmt.Sprintf("base(%d)", bb)
 		b.Run(name, func(b *testing.B) {
 			m := make(map[int]int)
+			b.ResetTimer()
+			for n := 0; n < b.N; n++ {
+				initMap(m, bb)
+			}
+			result = m
+		})
+
+		name = fmt.Sprintf("cap(%d)", bb)
+		b.Run(name, func(b *testing.B) {
+			m := make(map[int]int, bb)
 			b.ResetTimer()
 			for n := 0; n < b.N; n++ {
 				initMap(m, bb)
