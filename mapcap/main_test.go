593a38376d683456191969baaf845689be5d3b90
Davor Kapsa <davor.kapsa@gmail.com>
2017-05-05 19:13:40 +0200

    mapcap: add TestInitMap

--- mapcap/main_test.go
+++ mapcap/main_test.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestInitMap(t *testing.T) {
+	m := make(map[int]int)
+	n := 10
+	initMap(m, n)
+	got := len(m)
+	if got != n {
+		t.Errorf("initMap: len = %d; want %d", got, n)
+	}
+	fmt.Println(m)
+}
