40ff91f07065329da7e169713886ab994482cd0b
Davor Kapsa <davor.kapsa@gmail.com>
2017-05-06 21:17:04 +0200

    initfuncs: fix main

--- initfuncs/main.go
+++ initfuncs/main.go
@@ -1,11 +1,14 @@
 package main
 
+import "github.com/dvrkps/dojo/initfuncs/aaa"
+
 func init() {
 	println("main/main: top init")
 }
 
 func main() {
-	println("main/main: main")
+	println("main")
+	aaa.Aaa()
 }
 
 func init() {
