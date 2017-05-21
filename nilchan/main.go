853b7a5c9ba0eb0af50720353d40fff8b558fbd7
Davor Kapsa <davor.kapsa@gmail.com>
2017-05-21 20:03:24 +0200

    nilchan: add merge func

--- nilchan/main.go
+++ nilchan/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
 	a := gen("a", 3)
-	for i := range a {
+	b := gen("b", 5)
+	out := merge(a, b)
+	for i := range out {
 		println(i)
 	}
 }
@@ -32,10 +33,35 @@ func gen(name string, max int) chan string {
 	ch := make(chan string)
 	go func() {
 		for i := 1; i <= max; i++ {
-			time.Sleep(1e9)
+			//time.Sleep(1e9)
 			ch <- payload(name, i)
 		}
 		close(ch)
 	}()
 	return ch
+}
+
+func merge(a, b chan string) chan string {
+	out := make(chan string)
+	go func() {
+		var aClosed, bClosed bool
+		for !aClosed || !bClosed {
+			select {
+			case v, ok := <-a:
+				if !ok {
+					aClosed = true
+					continue
+				}
+				out <- v
+			case v, ok := <-b:
+				if !ok {
+					bClosed = true
+					continue
+				}
+				out <- v
+			}
+		}
+		close(out)
+	}()
+	return out
 }
