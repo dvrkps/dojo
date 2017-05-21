428ccfb150bc44c9accd5404db497a7a5ea6d0b9
Davor Kapsa <davor.kapsa@gmail.com>
2017-05-21 19:43:21 +0200

    nilchan: add gen

--- nilchan/main.go
+++ nilchan/main.go
@@ -1,21 +1,41 @@
 package main
 
-import "fmt"
-
-func main() {}
-
-const (
-	defaultName  = "empty"
-	defaultLimit = 10
-	minLimit     = 1
+import (
+	"fmt"
+	"time"
 )
 
-func payload(name string, max int) chan string {
+func main() {
+	a := gen("a", 3)
+	for i := range a {
+		println(i)
+	}
+}
+
+const (
+	defaultName = "empty"
+	minLimit    = 1
+	maxLimit    = 10
+)
+
+func payload(name string, max int) string {
 	if name == "" {
 		name = defaultName
 	}
 	if max < minLimit {
-		max = defaultMax
+		max = maxLimit
 	}
 	return fmt.Sprintf("%s%d", name, max)
+}
+
+func gen(name string, max int) chan string {
+	ch := make(chan string)
+	go func() {
+		for i := 1; i <= max; i++ {
+			time.Sleep(1e9)
+			ch <- payload(name, i)
+		}
+		close(ch)
+	}()
+	return ch
 }
