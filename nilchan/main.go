1197816d06913549c6336736485bafa56618a504
Davor Kapsa <davor.kapsa@gmail.com>
2017-05-21 19:34:02 +0200

    nilchan: add value func

--- nilchan/main.go
+++ nilchan/main.go
@@ -1,3 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {}
+
+const (
+	defaultName  = "empty"
+	defaultLimit = 10
+	minLimit     = 1
+)
+
+func value(name string, max int) chan string {
+	if name == "" {
+		name = defaultName
+	}
+	if max < minLimit {
+		max = defaultMax
+	}
+	return fmt.Sprintf("%s%d", name, max)
+}
