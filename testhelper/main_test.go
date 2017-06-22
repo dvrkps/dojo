02cf86d0d63dad216b0fc8c8a71a1b8de7b94276
Davor Kapsa <davor.kapsa@gmail.com>
2017-06-22 22:08:49 +0200

    testhelper: add testRange

--- testhelper/main_test.go
+++ testhelper/main_test.go
@@ -1,1 +1,17 @@
 package testhelper
+
+import "testing"
+
+var testCases = []struct {
+	in   []int
+	want int
+}{
+	{in: []int{1, 2}, want: 3},
+}
+
+func testRange(t *testing.T, fn func(t *testing.T, got, want int)) {
+	for _, tc := range testCases {
+		got := Sum(tc.in...)
+		fn(t, got, tc.want)
+	}
+}
