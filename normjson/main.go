5de15d0232b07a1d947cb9616226f42ff71f55ea
Davor Kapsa <davor.kapsa@gmail.com>
2017-06-24 22:20:42 +0200

    normjson: add T type

--- normjson/main.go
+++ normjson/main.go
@@ -1,1 +1,8 @@
 package normjson
+
+// T holds data.
+type T struct {
+	Label  interface{} `json:"label,omitempty"`
+	Active interface{} `json:"active,omitempty"`
+	Number interface{} `json:"number,omitempty"`
+}
