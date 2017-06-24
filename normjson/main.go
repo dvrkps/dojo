7e710b70853f23f48d5f008a36e57e6278018b34
Davor Kapsa <davor.kapsa@gmail.com>
2017-06-24 22:26:05 +0200

    normjson: add normalize func

--- normjson/main.go
+++ normjson/main.go
@@ -6,3 +6,34 @@ type T struct {
 	Active interface{} `json:"active,omitempty"`
 	Number interface{} `json:"number,omitempty"`
 }
+
+func normalize(t *T) error {
+	fields := []interface{}{
+		t.Label,
+		t.State,
+		t.Number,
+	}
+
+	for _, v := range fields {
+		switch v.(type) {
+		case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+			if v == 0 {
+				*v = nil
+			}
+		case float32, float64:
+			if *v == 0 {
+				*v = nil
+			}
+		case string:
+			if *v == "" {
+				*v = nil
+			}
+		}
+
+		//if *v == 0 || *v == "0" || *v == "false" || *v == false || *v == "" {
+		//	*v = nil
+		//}
+	}
+
+	return nil
+}
