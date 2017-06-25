db3ee3453b715d73565440057b73dd38f5d4cd8f
Davor Kapsa <davor.kapsa@gmail.com>
2017-06-25 20:08:03 +0200

    normjson: add T.normalize func

--- normjson/main.go
+++ normjson/main.go
@@ -1,13 +1,50 @@
 package normjson
 
+import (
+	"reflect"
+)
+
 // T holds data.
 type T struct {
+	I int64   `json:"i,omitempty"`
+	F float64 `json:"f,omitempty"`
+	B bool    `json:"b,omitempty"`
+	S string  `json:"s,omitempty"`
+
 	Label  interface{} `json:"label,omitempty"`
 	Active interface{} `json:"active,omitempty"`
 	Number interface{} `json:"number,omitempty"`
 }
 
-func normalize(t T) T {
+func (t *T) normalize() {
+	e := reflect.ValueOf(t).Elem()
+	//et := e.Type()
+	var f reflect.Value
+	for i, max := 0, e.NumField(); i < max; i++ {
+		f = e.Field(i)
+		if f.Type().String() != "interface {}" {
+			continue
+		}
+
+		v := f.Interface()
+		if v == "" ||
+			v == "undefined" ||
+			v == "null" ||
+			v == "NaN" ||
+			v == "0" ||
+			v == float64(0) ||
+			v == int64(0) ||
+			v == 0 ||
+			v == false ||
+			v == "false" {
+			f.Set(reflect.Zero(f.Type()))
+		}
+		//fmt.Printf("%q %q = %v\n",
+		//	et.Field(i).Name, f.Type(), f.Interface())
+	}
+}
+
+func old(t T) T {
 	fields := []interface{}{
 		t.Label,
 		t.Active,
