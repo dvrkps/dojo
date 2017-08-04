7ae55bac64cfbc272e8cb6f0a6d02d955778daf4
Davor Kapsa <davor.kapsa@gmail.com>
2017-08-04 18:10:57 +0200

    jj: add T

--- jj/main.go
+++ jj/main.go
@@ -1,3 +1,10 @@
 package main
 
 func main() {}
+
+type T struct {
+	I int64   `json:"i,omitempty"`
+	F float64 `json:"f,omitempty"`
+	B bool    `json:"b,omitempy"`
+	S []byte  `json:"s,omitempty"`
+}
