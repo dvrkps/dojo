26f37e1d153fcd839a57e539b8d0caaa910f197b
Davor Kapsa <davor.kapsa@gmail.com>
2017-08-04 18:58:11 +0200

    jj: add TestUnmarshal

--- jj/main_test.go
+++ jj/main_test.go
@@ -16,18 +16,24 @@ func testUnmarshalCases() map[string]testUnmarshalCase {
 	c := map[string]testUnmarshalCase{
 		"ok": {
 			ok:   true,
-			in:   []byte(`{i:123, f: 123.456, b:true,s:"abc"}`),
+			in:   []byte(`{"i":123, "f":123.456, "b":true,"s":"abc"}`),
 			want: T{I: int64(123), F: float64(123.456), B: true, S: []byte{'a', 'b', 'c'}},
 		},
 	}
 	return c
 }
 
-func TestUnmarshal(t *testing.T) {}
+func TestUnmarshal(t *testing.T) {
+	for name, tc := range testUnmarshalCases() {
+		t.Run(name, func(t *testing.T) {
+			testUnmarshal(t, tc)
+		})
+	}
+}
 
 func testUnmarshal(t *testing.T, tc testUnmarshalCase) {
 	var got T
-	err := json.Unmarshal(tc.in, got)
+	err := json.Unmarshal(tc.in, &got)
 	if !tc.ok {
 		if !reflect.DeepEqual(got, tc.want) || err == nil {
 			t.Errorf("got %+v, %v; want %+v, <error>", got, err, tc.want)
