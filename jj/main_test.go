80abdfa5db3e6999820f5b95bf43b6bd0a407ecd
Davor Kapsa <davor.kapsa@gmail.com>
2017-08-04 18:24:19 +0200

    jj: add testUnmarshalCases

--- jj/main_test.go
+++ jj/main_test.go
@@ -8,4 +8,15 @@ type testUnmarshalCase struct {
 	want interface{}
 }
 
+func testUnmarshalCases() map[string]testUnmarshalCase {
+	c := map[string]testUnmarshalCase{
+		"ok": {
+			ok:   true,
+			in:   []byte(`{i:123, f: 123.456, b:true,s:"abc"}`),
+			want: T{I: int64(123), F: float64(123.456), B: true, S: []byte{'a', 'b', 'c'}},
+		},
+	}
+	return c
+}
+
 func TestUnmarshal(t *testing.T) {}
