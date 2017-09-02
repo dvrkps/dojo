85164b024009fd04c4b4e2cdb2a04bdecbb609db
Davor Kapsa <davor.kapsa@gmail.com>
2017-09-02 19:57:19 +0200

    fromquotes: add TestOriginal

--- fromquotes/main_test.go
+++ fromquotes/main_test.go
@@ -1,16 +1,37 @@
 package main
 
+import (
+	"reflect"
+	"testing"
+)
+
 type testCase struct {
 	in  []string
 	out []string
 }
 
-func testCases() map[string]testCase {
-	m := map[string]testCase{
-		"short": []string{".hi", "\"My", "name", "is", "Omar\"", "123", "\"123\""},
-		"long":  []string{"\".hi", "I'm", "the", "real", "Slim", "Shady", "\"My", "name", "is", "Omar\"", "hello", "world", "\"123\"", "a"},
+func testCases() []testCase {
+	r := []testCase{
+		{
+			in:  []string{".hi", "\"My", "name", "is", "Omar\"", "\"123\""},
+			out: []string{".hi", "My name is Omar", "\"123\""},
+		},
+		{
+			in:  []string{"\".hi", "I'm", "the", "real", "Slim", "Shady", "\"My", "name", "is", "Omar\"", "hello", "world", "\"123\"", "a"},
+			out: []string{".hi I'm the real Slim ShadyMy name is Omar", "hello", "world", "\"123\"", "a"},
+		},
 	}
-	return m
+	return r
 }
 
 var result []string
+
+func TestOriginal(t *testing.T) {
+	for _, tc := range testCases() {
+		got := original(tc.in)
+		want := tc.out
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("original(%v) = %v; want %v", got, want)
+		}
+	}
+}
