7cb92096b6d7261c7b357e3b6a2c5af72448281a
Davor Kapsa <davor.kapsa@gmail.com>
2017-09-02 21:46:50 +0200

    fromqoutes: move original to file

---
+++ fromquotes/original.go
@@ -0,0 +1,42 @@
+package main
+
+import "bytes"
+
+// Original finds a "string which spans multiple spaces" in a split message.
+// Then takes that and replaces the Quote string with a single string value of the quote contents.
+func original(parts []string) []string {
+	// https://play.golang.org/p/908AF_h0q-
+	found := false
+	var buffer bytes.Buffer
+	var newParts []string
+
+	for _, val := range parts {
+		if !found {
+			if val[0] == '"' {
+				if val[len(val)-1] == '"' {
+					found = true
+					val = val[:len(val)-1]
+					buffer.WriteString(val[1:])
+					newParts = append(newParts, buffer.String())
+				} else {
+					buffer.WriteString(val[1:])
+				}
+			} else if buffer.Len() != 0 {
+				if val[len(val)-1] == '"' {
+					found = true
+					val = val[:len(val)-1]
+					buffer.WriteString(" " + val)
+					newParts = append(newParts, buffer.String())
+				} else {
+					buffer.WriteString(" " + val)
+				}
+			} else {
+				newParts = append(newParts, val)
+			}
+		} else {
+			newParts = append(newParts, val)
+		}
+	}
+
+	return newParts
+}
