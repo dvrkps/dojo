044ed8d102273d0c21b64c6f6119abb9e3af9a4e
Davor Kapsa <davor.kapsa@gmail.com>
2017-09-02 21:36:30 +0200

    fromquotes: add Benchmark

--- fromquotes/benchmarks_test.go
+++ fromquotes/benchmarks_test.go
@@ -1,13 +1,15 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 var result []string
 
-func benchCases() map[string][]string {
-	m := map[string][]string{
-		"short": short(),
-		"long":  long(),
+func benchCases() map[string]func([]string) []string {
+	m := map[string]func([]string) []string{
+		"original": original,
 	}
 	return m
 }
@@ -19,4 +21,20 @@ func benchFunc(b *testing.B, fn func([]string) []string, in []string) {
 		r = fn(in)
 	}
 	result = r
+}
+
+func Benchmark(b *testing.B) {
+	inputs := map[string][]string{
+		"short": short(),
+		"long":  long(),
+	}
+	for bname, fn := range benchCases() {
+		for iname, in := range inputs {
+			name := fmt.Sprintf("%s(%s)", bname, iname)
+			b.Run(name, func(b *testing.B) {
+				benchFunc(b, fn, in)
+			})
+		}
+
+	}
 }
