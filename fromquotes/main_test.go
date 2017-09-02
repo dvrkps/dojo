1c127b828c8825a214c6a06524996120d9ae3011
Davor Kapsa <davor.kapsa@gmail.com>
2017-09-02 19:42:36 +0200

    fromquotes: add testCases

--- fromquotes/main_test.go
+++ fromquotes/main_test.go
@@ -4,3 +4,13 @@ type testCase struct {
 	in  []string
 	out []string
 }
+
+func testCases() map[string]testCase {
+	m := map[string]testCase{
+		"short": []string{".hi", "\"My", "name", "is", "Omar\"", "123", "\"123\""},
+		"long":  []string{"\".hi", "I'm", "the", "real", "Slim", "Shady", "\"My", "name", "is", "Omar\"", "hello", "world", "\"123\"", "a"},
+	}
+	return m
+}
+
+var result []string
