1bce465b9c06ea60222e3990a1dd50d13dad031f
Davor Kapsa <davor.kapsa@gmail.com>
2017-09-02 19:15:04 +0200

    fromquotes: add long func

--- fromquotes/main.go
+++ fromquotes/main.go
@@ -5,3 +5,7 @@ func main() {}
 func short() []string {
 	return []string{".hi", "\"My", "name", "is", "Omar\"", "123", "\"123\""}
 }
+
+func long() []string {
+	return []string{"\".hi", "I'm", "the", "real", "Slim", "Shady", "\"My", "name", "is", "Omar\"", "hello", "world", "\"123\"", "a"}
+}
