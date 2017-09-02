bbedfc184a04d8bf0ad1db843dbea42173e25fde
Davor Kapsa <davor.kapsa@gmail.com>
2017-09-02 21:49:00 +0200

    add better func

---
+++ fromquotes/better.go
@@ -0,0 +1,40 @@
+package main
+
+import "bytes"
+
+func better(parts []string) []string {
+	// https://play.golang.org/p/908AF_h0q-
+	found := false
+	var buffer bytes.Buffer
+	var newParts []string
+
+	for _, val := range parts {
+		if !found {
+			if val[0] == '"' {
+				if val[len(val)-1] == '"' {
+					found = true
+					val = val[:len(val)-1]
+					buffer.WriteString(val[1:])
+					newParts = append(newParts, buffer.String())
+				} else {
+					buffer.WriteString(val[1:])
+				}
+			} else if buffer.Len() != 0 {
+				if val[len(val)-1] == '"' {
+					found = true
+					val = val[:len(val)-1]
+					buffer.WriteString(" " + val)
+					newParts = append(newParts, buffer.String())
+				} else {
+					buffer.WriteString(" " + val)
+				}
+			} else {
+				newParts = append(newParts, val)
+			}
+		} else {
+			newParts = append(newParts, val)
+		}
+	}
+
+	return newParts
+}
