58e0be2c89dc929cd6b397a15e23aedb71c3172d
Davor Kapsa <davor.kapsa@gmail.com>
2017-09-04 13:55:19 +0200

    benchregexp: add BenchmarkWithRegexp

--- benchregexp/main_test.go
+++ benchregexp/main_test.go
@@ -6,5 +6,14 @@ func benchQuery() map[string]string {
 	return map[string]string{"sv": "value of sv"}
 }
 
+var result bool
+
 func BenchmarkWihRegexp(b *testing.B) {
+	var r bool
+	q := benchQuery()
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		r = withRegexp(q)
+	}
+	result = r
 }
