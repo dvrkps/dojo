857f35a40b76f076af5b9e1e8b5a60c49039c957
Davor Kapsa <davor.kapsa@gmail.com>
2017-09-04 12:56:07 +0200

    fromquotes: clean better

--- fromquotes/better.go
+++ fromquotes/better.go
@@ -1,39 +1,50 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"strings"
+)
 
-func better(parts []string) []string {
+func better(in []string) []string {
 	found := false
 	var buffer bytes.Buffer
-	var newParts []string
+	out := make([]string, 0, len(in))
 
-	for _, val := range parts {
+	const q string = `"`
+
+	var size, lastPos int
+	//var first, last byte
+	for _, row := range in {
+		size = len(row)
+		lastPos = size - 1
+		//first = row[0]
+		//last = row[lastPos]
 		if found {
-			newParts = append(newParts, val)
+			out = append(out, row)
 			continue
 		}
-		if val[0] == '"' {
-			if val[len(val)-1] == '"' {
+		if strings.HasPrefix(row, q) {
+			if strings.HasSuffix(row, q) {
 				found = true
-				val = val[:len(val)-1]
-				buffer.WriteString(val[1:])
-				newParts = append(newParts, buffer.String())
+				row = row[:lastPos]
+				buffer.WriteString(row[1:])
+				out = append(out, buffer.String())
 			} else {
-				buffer.WriteString(val[1:])
+				buffer.WriteString(row[1:])
 			}
 		} else if buffer.Len() != 0 {
-			if val[len(val)-1] == '"' {
+			if strings.HasSuffix(row, q) {
 				found = true
-				val = val[:len(val)-1]
-				buffer.WriteString(" " + val)
-				newParts = append(newParts, buffer.String())
+				row = row[:lastPos]
+				buffer.WriteString(" " + row)
+				out = append(out, buffer.String())
 			} else {
-				buffer.WriteString(" " + val)
+				buffer.WriteString(" " + row)
 			}
 		} else {
-			newParts = append(newParts, val)
+			out = append(out, row)
 		}
 	}
 
-	return newParts
+	return out
 }
