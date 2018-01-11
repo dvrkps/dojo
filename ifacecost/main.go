c1aa271717c6356326a7c43a27913dc2eb1f9f58
Davor Kapsa <davor.kapsa@gmail.com>
2018-01-11 19:16:51 +0100

    ifacecost: clean

--- ifacecost/main.go
+++ ifacecost/main.go
@@ -1,9 +1,36 @@
 package main
 
-type Number struct {
-	v int
+import (
+	"fmt"
+	"strings"
+)
+
+type words struct {
+	sentence string
 }
 
-func (n Numer) Add(x int) int {
-	n.v += x
+func (w words) split() []string {
+	return strings.Fields(w.sentence)
+}
+
+type spliter interface {
+	split() []string
+}
+
+func typeSplit(w words) []string {
+	return w.split()
+}
+
+func ifaceSplit(s spliter) []string {
+	return s.split()
+}
+
+func main() {
+	w := words{sentence: "aaa bbb ccc"}
+
+	ts := typeSplit(w)
+	fmt.Println(ts)
+
+	is := ifaceSplit(w)
+	fmt.Println(is)
 }
