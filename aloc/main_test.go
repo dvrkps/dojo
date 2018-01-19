6be14479471ec1d35868da01595486ab13a864d9
Davor Kapsa <davor.kapsa@gmail.com>
2018-01-19 13:33:50 +0100

    aloc: add BenchmarkCAdd

---
+++ aloc/main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+var result []int
+
+func BenchmarkCAdd(b *testing.B) {
+	var r []int
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		r = cAdd(r, 1)
+		r = cAdd(r, 1, 2, 3)
+	}
+	result = r
+}
