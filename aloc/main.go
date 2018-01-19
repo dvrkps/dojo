34fcf1d53cc7fffd8cb0beb310e8c8fefc5b7f8d
Davor Kapsa <davor.kapsa@gmail.com>
2018-01-19 13:36:15 +0100

    aloc: add pAdd

--- aloc/main.go
+++ aloc/main.go
@@ -7,11 +7,16 @@ func main() {
 	show(s)
 
 	s = cAdd(s, 1, 2, 3)
+	pAdd(&s, 11, 22, 33)
 	show(s)
 }
 
 func cAdd(s []int, v ...int) []int {
 	return append(s, v...)
+}
+
+func pAdd(s *[]int, v ...int) {
+	*s = append(*s, v...)
 }
 
 func show(s []int) {
