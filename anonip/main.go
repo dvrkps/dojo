9418dc7dbd29fb52e14e79ddd0b4e3cc8be3521d
Davor Kapsa <davor.kapsa@gmail.com>
2018-05-15 16:27:57 +0200

    clean AnonymizeIP

--- anonip/main.go
+++ anonip/main.go
@@ -8,14 +8,14 @@ func AnonymizeIP(rawIP string) net.IP {
 	ip := net.ParseIP(rawIP)
 	if ip == nil {
 		// invalid ip
-		return ip
+		return nil
 	}
 	if ip.To4() != nil {
 		// IPv4
 		mask := net.CIDRMask(16, 32)
 		return ip.Mask(mask)
 	}
-
+	// IPv6
 	mask := net.CIDRMask(48, 128)
 	return ip.Mask(mask)
 }
