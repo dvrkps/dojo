89c43b1d23e8458ccfd3af55c3a9273524e883e2
Davor Kapsa <davor.kapsa@gmail.com>
2018-05-15 16:14:36 +0200

    add AnonymizeIP

--- anonip/main.go
+++ anonip/main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"net"
+)
+
+func AnonymizeIP(rawIP string) net.IP {
+	ip := net.ParseIP(rawIP)
+	if ip == nil {
+		// invalid ip
+		return ip
+	}
+	if ip.To4() != nil {
+		// IPv4
+		mask := net.CIDRMask(16, 32)
+		return ip.Mask(mask)
+	}
+
+	mask := net.CIDRMask(48, 128)
+	return ip.Mask(mask)
+}
