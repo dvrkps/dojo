adb0b54544c0774b3ea6cca68a2b7f1528f03d40
Davor Kapsa <davor.kapsa@gmail.com>
2018-05-15 13:52:29 +0200

    anonip: add TestAnonymizeIP

--- anonip/main_test.go
+++ anonip/main_test.go
@@ -1,1 +1,22 @@
 package main
+
+import "testing"
+
+func TestAnonymizeIP(t *testing.T) {
+	cases := []struct {
+		in   string
+		want string
+	}{
+		{
+			in:   "8.8.8.8",
+			want: "8.8.0.0",
+		},
+	}
+	for _, tc := range cases {
+		got := AnonymizeIP(tc.in)
+		if got.String() != tc.want {
+			t.Errorf("AnonymizeIP( %s ) = %s; want %s",
+				tc.in, got.String(), tc.want)
+		}
+	}
+}
