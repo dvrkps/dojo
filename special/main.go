0f73c6b0cac29c080aa7e4779a83769f907fad6f
Davor Kapsa <davor.kapsa@gmail.com>
2018-09-04 18:08:47 +0200

    special: add naive

--- special/main.go
+++ special/main.go
@@ -1,1 +1,12 @@
-package main
+package special
+
+func naive(n uint32) bool {
+	for wasLastBitOne := false; n > 0; n = n >> 1 {
+		isCurrentBitOne := n%2 == 1
+		if isCurrentBitOne && wasLastBitOne {
+			return true
+		}
+		wasLastBitOne = isCurrentBitOne
+	}
+	return false
+}
