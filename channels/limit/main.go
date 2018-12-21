28af60d00c103659fb5bbb03b976fd5c42032f20
Davor Kapsa <davor.kapsa@gmail.com>
2018-12-21 15:11:36 +0100

    channels/limit: add main and worker

--- channels/limit/main.go
+++ channels/limit/main.go
@@ -1,3 +1,35 @@
 package main
 
-func main() {}
+import "time"
+
+func main() {
+	const (
+		cap = 4
+		max = 6
+	)
+	ch := make(chan int, cap)
+	go worker(ch)
+	for i := 0; i < max; i++ {
+		ch <- i
+		println("add", i)
+	}
+	close(ch)
+	println("end of sending")
+	time.Sleep(3e9)
+}
+
+func worker(in chan int) {
+	for {
+		select {
+		case ch, ok := <-in:
+			if !ok {
+				println("end")
+				return
+			}
+			time.Sleep(1e6)
+			println(ch, len(in), "/", cap(in))
+		default:
+			println("default")
+		}
+	}
+}
