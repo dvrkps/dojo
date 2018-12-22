dc1fe7dbacae88c22c5ef32d66bcb4c04860e6d4
Davor Kapsa <davor.kapsa@gmail.com>
2018-12-22 01:47:08 +0100

    channels/limit: select on send

--- channels/limit/main.go
+++ channels/limit/main.go
@@ -5,31 +5,27 @@ import "time"
 func main() {
 	const (
 		cap = 4
-		max = 6
+		max = 8
 	)
 	ch := make(chan int, cap)
 	go worker(ch)
 	for i := 0; i < max; i++ {
-		ch <- i
-		println("add", i)
+		select {
+		case ch <- i:
+			println("send", i)
+		default:
+			println("block", i)
+			ch <- i
+		}
 	}
 	close(ch)
 	println("end of sending")
 	time.Sleep(3e9)
 }
 
-func worker(in chan int) {
-	for {
-		select {
-		case ch, ok := <-in:
-			if !ok {
-				println("chan close")
-				return
-			}
-			time.Sleep(1e6)
-			println(ch, len(in), "/", cap(in))
-		default:
-			println("default")
-		}
+func worker(ch chan int) {
+	for v := range ch {
+		time.Sleep(1e6)
+		println("receive", v)
 	}
 }
