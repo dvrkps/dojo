7714cebf8d4415908b35d0b0e94da8b3dc7e0f9e
Davor Kapsa <davor.kapsa@gmail.com>
2019-03-13 12:02:33 +0100

    davelog: add New

--- davelog/davelog_test.go
+++ davelog/davelog_test.go
@@ -2,3 +2,23 @@
 // Inspired by article of Dave Cheney.
 // https://dave.cheney.net/2015/11/05/lets-talk-about-logging
 package davelog
+
+import (
+	"io"
+	"log"
+)
+
+// A Log represents an active logger.
+type Log struct {
+	log     *log.Logger
+	verbose bool
+}
+
+// New creates logger.
+func New(w io.Writer, verbose bool) *Log {
+	l := Log{
+		log:     log.New(w, "", 0),
+		verbose: verbose,
+	}
+	return &l
+}
