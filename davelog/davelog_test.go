a77d3e71ea99af2e5eb3dad130a4ef79647df02f
Davor Kapsa <davor.kapsa@gmail.com>
2019-03-13 12:18:05 +0100

    davelog: add TestNew

--- davelog/davelog_test.go
+++ davelog/davelog_test.go
@@ -3,42 +3,11 @@
 // https://dave.cheney.net/2015/11/05/lets-talk-about-logging
 package davelog
 
-import (
-	"io"
-	"log"
-)
+import "testing"
 
-// A Log represents an active logger.
-type Log struct {
-	output  *log.Logger
-	verbose bool
-}
-
-// New creates logger.
-func New(w io.Writer, verbose bool) *Log {
-	l := Log{
-		output:  log.New(w, "", 0),
-		verbose: verbose,
+func TestNew(t *testing.T) {
+	l := New(nil, false)
+	if l == nil {
+		t.Error("New( ... ) == nil")
 	}
-	return &l
-}
-
-// Infof logs formatted info message.
-func (l *Log) Infof(format string, v ...interface{}) {
-	l.logf(format, v...)
-}
-
-// Debugf logs formatted debug message.
-func (l *Log) Debugf(format string, v ...interface{}) {
-	if !l.verbose {
-		return
-	}
-	l.logf(format, v...)
-}
-
-func (l *Log) logf(format string, v ...interface{}) {
-	if l.output == nil {
-		return
-	}
-	l.output.Printf(format, v...)
 }
