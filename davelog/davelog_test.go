261572096010c00563ec975fc8ca6bd52737b78f
Davor Kapsa <davor.kapsa@gmail.com>
2019-03-13 12:50:59 +0100

    davelog: add TestLog

--- davelog/davelog_test.go
+++ davelog/davelog_test.go
@@ -3,11 +3,62 @@
 // https://dave.cheney.net/2015/11/05/lets-talk-about-logging
 package davelog
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestNew(t *testing.T) {
 	l := New(nil, false)
 	if l == nil {
 		t.Error("New( ... ) == nil")
+	}
+}
+
+func testSetup(verbose bool) (*Log, *bytes.Buffer) {
+	var buf = &bytes.Buffer{}
+	return New(buf, verbose), buf
+}
+
+func TestLog(t *testing.T) {
+	tests := []struct {
+		name    string
+		verbose bool
+		action  func(log *Log)
+		want    string
+	}{
+		{
+			name:    "infof",
+			verbose: false,
+			action: func(log *Log) {
+				log.Infof("info %v", 42)
+			},
+			want: "info 42\n",
+		},
+		{
+			name:    "debugf",
+			verbose: true,
+			action: func(log *Log) {
+				log.Debugf("debug %v", 42)
+			},
+			want: "debug 42\n",
+		},
+		{
+			name:    "debugf",
+			verbose: false,
+			action: func(log *Log) {
+				log.Debugf("debug %v", 42)
+			},
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		log, buf := testSetup(tt.verbose)
+		tt.action(log)
+		got := buf.String()
+		if got != tt.want {
+			t.Errorf("%s: = %q; want %q",
+				tt.name, got, tt.want)
+		}
 	}
 }
