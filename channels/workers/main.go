49901529a1df9a634e082362f1fb09c6a24d803a
Davor Kapsa <davor.kapsa@gmail.com>
2019-07-15 16:51:31 +0200

    channels/workers: init

---
+++ channels/workers/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"sync"
+)
+
+func main() {
+	const (
+		max       = 4
+		noWorkers = 10
+	)
+	ch := make(chan int, 1)
+
+	wg := startWorkers(noWorkers, ch)
+	for i := 0; i < max; i++ {
+		select {
+		case ch <- i:
+			println("send", i)
+		default:
+			println("*** block", i)
+			ch <- i
+		}
+	}
+	close(ch)
+	println("sending done")
+	wg.Wait()
+	println("the end")
+}
+
+func startWorkers(max int, ch chan int) *sync.WaitGroup {
+	initWG := &sync.WaitGroup{}
+	initWG.Add(max)
+	wg := &sync.WaitGroup{}
+	wg.Add(max)
+	for i := 0; i < max; i++ {
+		go worker(initWG, wg, ch)
+	}
+	initWG.Wait()
+	println("workers init")
+	return wg
+}
+
+func worker(initWG *sync.WaitGroup, wg *sync.WaitGroup, ch chan int) {
+	initWG.Done()
+	for v := range ch {
+		//time.Sleep(1e9)
+		println("receive", v)
+	}
+	wg.Done()
+	println("end worker")
+}
