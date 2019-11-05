d9220fb5a562f10fdc35584515c0b60ad797fbde
Davor Kapsa <davor.kapsa@gmail.com>
2019-11-05 19:19:53 +0100

    rest: add run func

--- rest/main.go
+++ rest/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"io"
+	"os"
+)
+
 func main() {
+	os.Exit(run(os.Args, os.Stdout, os.Stderr))
+}
+
+const (
+	exitOk   = 0
+	exitErr  = 1
+	exitUser = 2
+)
+
+func run(args []string, stdout, stderr io.Writer) int {
+	return exitOk
 }
