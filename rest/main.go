ca1288a04243d965689f72c6c69a4b94ecdf5c29
Davor Kapsa <davor.kapsa@gmail.com>
2019-12-04 19:18:11 +0100

    rest: add log and config to run

--- rest/main.go
+++ rest/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
+
+	"github.com/dvrkps/dojo/rest/internal/configuration"
+	"github.com/dvrkps/dojo/rest/internal/log"
 )
 
 func main() {
@@ -11,10 +15,22 @@ func main() {
 
 const (
 	exitOk = 0
-	// exitErr  = 1
-	// exitUser = 2
+	//exitErr  = 1
+	exitUser = 2
 )
 
 func run(args []string, stdout, stderr io.Writer) int {
+	config, err := configuration.New(args, stderr)
+	if err != nil {
+		fmt.Fprintf(stderr, "configuration: %v", err)
+		return exitUser
+	}
+
+	log := log.New(config.Verbose, stdout, stderr)
+
+	log.Infof("%v", "info")
+	log.Debugf("%v", "debug")
+	log.Errorf("%v", "err")
+
 	return exitOk
 }
