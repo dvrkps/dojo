272aa828f453936f99dbd225f1bc3bd95a583e18
Davor Kapsa <davor.kapsa@gmail.com>
2020-01-15 22:17:29 +0100

    rest/internal/handlers: add json content type helper

--- rest/internal/handlers/api.go
+++ rest/internal/handlers/api.go
@@ -15,8 +15,9 @@ type API struct {
 // Routes inits all api routes.
 func (a *API) Routes() http.Handler {
 	m := http.NewServeMux()
-	m.HandleFunc("/a", aecho)
-	m.HandleFunc("/b", becho)
+	m.HandleFunc("/a", jsonContentType(aecho))
+	m.HandleFunc("/b", jsonContentType(becho))
+
 	a.mux = m
 
 	return a
@@ -28,11 +29,20 @@ func (a *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func aecho(w http.ResponseWriter, r *http.Request) {
-	n := rand.Intn(1000)
+	const randMax = 1000
+	n := rand.Intn(randMax)
 	fmt.Fprintf(w, "a) You asked to %s %s result: %d\n", r.Method, r.URL.Path, n)
 }
 
 func becho(w http.ResponseWriter, r *http.Request) {
-	n := rand.Intn(1000)
+	const randMax = 1000
+	n := rand.Intn(randMax)
 	fmt.Fprintf(w, "b) You asked to %s %s result: %d\n", r.Method, r.URL.Path, n)
+}
+
+func jsonContentType(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		next(w, r)
+	}
 }
