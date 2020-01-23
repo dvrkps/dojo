955ca9d55d09e795864a5f07d501858bea49084d
Davor Kapsa <davor.kapsa@gmail.com>
2020-01-23 20:10:40 +0100

    rest/internal/handlers: better auth tests

--- rest/internal/handlers/auth.go
+++ rest/internal/handlers/auth.go
@@ -2,13 +2,19 @@ package handlers
 
 import (
 	"net/http"
+
+	"github.com/dvrkps/dojo/rest/internal/authorization"
 )
 
 func (a *API) withAuth(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		//fmt.Printf("%+v", r.Header)
-		//w.WriteHeader(http.StatusUnauthorized)
-		//return
+		err := authorization.Validate(r.Header)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+
+			return
+		}
+
 		next(w, r)
 	}
 }
--- rest/internal/handlers/auth_test.go
+++ rest/internal/handlers/auth_test.go
@@ -7,19 +7,64 @@ import (
 )
 
 func TestAuth(t *testing.T) {
-	req := httptest.NewRequest("GET", "/", nil)
-	req.Header.Set("Date", "Tue, 07 Jun 2011 20:51:35 GMT")
-	req.Header.Set("Authorization", `algorithm="hmac-sha256",headers="date",signature="UDysfR6MndUZReo07Y9r+vErn8vSxrnQ5ulit18iJ/Q=",apikey="_here_is_the_api_key_"`)
-	w := httptest.NewRecorder()
-	dummy := func(w http.ResponseWriter, r *http.Request) {}
-	a := API{}
-	handler := a.withAuth(dummy)
-	handler(w, req)
+	for _, tt := range authTests() {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			r := httptest.NewRequest("GET", "/", nil)
+			r.Header = tt.header
 
-	got := w.Result()
+			w := httptest.NewRecorder()
 
-	const want = http.StatusOK
-	if got.StatusCode != want {
-		t.Errorf("withAuth status code = %v; want %v", got.StatusCode, want)
+			dummy := func(w http.ResponseWriter, r *http.Request) {}
+
+			a := API{}
+			handler := a.withAuth(dummy)
+			handler(w, r)
+
+			got := w.Result()
+			defer got.Body.Close()
+
+			want := tt.statusCode
+			if got.StatusCode != want {
+				t.Errorf("withAuth status code = %v; want %v", got.StatusCode, want)
+			}
+		})
 	}
+}
+
+type authTest struct {
+	name       string
+	statusCode int
+	header     http.Header
+}
+
+const testSignatureValue = "druUFhOEGiAir3wspPxJF3mRU5CaXP0J7LIrN2tVDV8="
+
+func authTests() []authTest {
+	tests := []authTest{
+		{
+			name:       "valid",
+			statusCode: http.StatusOK,
+			header: http.Header{
+				"Date": []string{"Tue, 07 Jun 2011 20:51:35 GMT"},
+				"Authorization": []string{
+					`algorithm="hmac-sha256",` +
+						`headers="date",` +
+						`signature="` +
+						testSignatureValue +
+						`",` +
+						`apikey="_here_is_the_api_key_"`,
+				},
+			},
+		},
+		{
+			name:       "no authorization header",
+			statusCode: http.StatusUnauthorized,
+			header: http.Header{
+				"Date": []string{"Tue, 07 Jun 2011 20:51:35 GMT"},
+			},
+		},
+	}
+
+	return tests
 }
