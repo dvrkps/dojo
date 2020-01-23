3c0fb5c6a101d327b62b3e59e8a2c29e9b8af53c
Davor Kapsa <davor.kapsa@gmail.com>
2020-01-23 20:37:16 +0100

    rest/internal/handlers: add json middleware

--- rest/internal/handlers/api.go
+++ rest/internal/handlers/api.go
@@ -14,9 +14,18 @@ type API struct {
 
 // Routes inits all api routes.
 func (a *API) Routes() http.Handler {
+	dummy := func(name string) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			const randMax = 1000
+			n := rand.Intn(randMax)
+			fmt.Fprintf(w, "%v: %v\n", name, n)
+		}
+	}
+
 	m := http.NewServeMux()
-	m.HandleFunc("/a", a.withContentTypeJSON(aecho))
-	m.HandleFunc("/b", a.withContentTypeJSON(becho))
+
+	m.HandleFunc("/a", a.withJSON(dummy("A")))
+	m.HandleFunc("/b", a.withJSON(dummy("B")))
 
 	a.mux = m
 
@@ -26,23 +35,4 @@ func (a *API) Routes() http.Handler {
 // ServeHTTP implements http.Handler.
 func (a *API) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	a.mux.ServeHTTP(w, r)
-}
-
-func aecho(w http.ResponseWriter, r *http.Request) {
-	const randMax = 1000
-	n := rand.Intn(randMax)
-	fmt.Fprintf(w, "a) You asked to %s %s result: %d\n", r.Method, r.URL.Path, n)
-}
-
-func becho(w http.ResponseWriter, r *http.Request) {
-	const randMax = 1000
-	n := rand.Intn(randMax)
-	fmt.Fprintf(w, "b) You asked to %s %s result: %d\n", r.Method, r.URL.Path, n)
-}
-
-func (a *API) withContentTypeJSON(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		next(w, r)
-	}
 }
