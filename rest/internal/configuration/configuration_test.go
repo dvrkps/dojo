69f9da7a36cdbc87d63d58f7fcc51efdaadcb49d
Davor Kapsa <davor.kapsa@gmail.com>
2020-01-23 19:13:12 +0100

    rest/internal/configuration: clean tests

--- rest/internal/configuration/configuration_test.go
+++ rest/internal/configuration/configuration_test.go
@@ -7,11 +7,47 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	tests := []struct {
-		fail bool
-		args string
-		want *Configuration
-	}{
+	for _, tt := range newTests() {
+		tt := tt
+		t.Run(tt.args, func(t *testing.T) {
+			args := strings.Fields(tt.args)
+
+			var buf bytes.Buffer
+
+			got, err := New(args, &buf)
+
+			if tt.fail {
+				if err == nil {
+					t.Errorf("fail: got %v want <error>", err)
+				}
+				return
+			}
+			compareTestConfigurations(t, got, tt.want)
+			if err != nil {
+				t.Errorf("compare: got %v want <nil>", err)
+			}
+		})
+	}
+}
+
+func compareTestConfigurations(t *testing.T, got, want *Configuration) {
+	if got.Addr != want.Addr {
+		t.Errorf("addr: got %v; want %v", got.Addr, want.Addr)
+	}
+
+	if got.Verbose != want.Verbose {
+		t.Errorf("verbose: got %v; want %v", got.Verbose, want.Verbose)
+	}
+}
+
+type newTest = struct {
+	fail bool
+	args string
+	want *Configuration
+}
+
+func newTests() []newTest {
+	tests := []newTest{
 		{
 			args: "cmd",
 			want: &Configuration{
@@ -28,35 +64,5 @@ func TestNew(t *testing.T) {
 		},
 	}
 
-	for _, tt := range tests {
-		args := strings.Fields(tt.args)
-
-		var buf bytes.Buffer
-
-		got, err := New(args, &buf)
-		want := tt.want
-		fail := tt.fail
-		t.Run(tt.args, func(t *testing.T) {
-			if fail {
-				if err == nil {
-					t.Errorf("fail: got %v want <error>", err)
-				}
-				return
-			}
-			compareTestConfigurations(t, got, want)
-			if err != nil {
-				t.Errorf("compare: got %v want <nil>", err)
-			}
-		})
-	}
-}
-
-func compareTestConfigurations(t *testing.T, got, want *Configuration) {
-	if got.Addr != want.Addr {
-		t.Errorf("addr: got %v; want %v", got.Addr, want.Addr)
-	}
-
-	if got.Verbose != want.Verbose {
-		t.Errorf("verbose: got %v; want %v", got.Verbose, want.Verbose)
-	}
+	return tests
 }
