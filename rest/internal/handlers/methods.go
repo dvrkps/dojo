9a900418d52e913231bab0a868f1d0d6d32d8af4
Davor Kapsa <davor.kapsa@gmail.com>
2020-01-28 20:51:01 +0100

    rest/internal/handlers: add checkMethod

--- rest/internal/handlers/methods.go
+++ rest/internal/handlers/methods.go
@@ -3,23 +3,21 @@ package handlers
 import "net/http"
 
 func (a *API) withGet(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-
-			return
-		}
-		next(w, r)
-	}
+	return checkMethod(http.MethodGet, next)
 }
 
 func (a *API) withPost(next http.HandlerFunc) http.HandlerFunc {
+	return checkMethod(http.MethodPost, next)
+}
+
+func checkMethod(methodName string, next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
+		if r.Method != methodName {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 
 			return
 		}
+
 		next(w, r)
 	}
 }
