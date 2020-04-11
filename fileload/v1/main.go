fa75f950c260467367be12a0f09c7b8d949fc554
Davor Kapsa <davor.kapsa@gmail.com>
2020-04-11 16:47:43 +0200

    fileload: add v1

---
+++ fileload/v1/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/dvrkps/dojo/fileload"
+)
+
+func main() {
+	d, err := run()
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+	fmt.Print(&d)
+}
+
+func run() (fileload.Data, error) {
+	empty := fileload.Data{}
+
+	c, err := ioutil.ReadFile(fileload.Path)
+	if err != nil {
+		return empty, fmt.Errorf("read file: %v", err)
+	}
+
+	r := bytes.NewReader(c)
+
+	d := fileload.NewData()
+
+	err = parse(r, &d)
+	if err != nil {
+		return empty, fmt.Errorf("parse: %v", err)
+	}
+
+	return d, nil
+}
+
+func parse(r io.Reader, d *fileload.Data) error {
+	s := bufio.NewScanner(r)
+
+	for s.Scan() {
+		err := d.Add(s.Bytes())
+		if err != nil {
+			return fmt.Errorf("add: %v", err)
+		}
+	}
+
+	return s.Err()
+}
