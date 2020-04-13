de395ac2629bc2716e65e5831c1d753d5d8a6d62
Davor Kapsa <davor.kapsa@gmail.com>
2020-04-13 20:53:46 +0200

    fileload: clean data and row

--- fileload/data.go
+++ fileload/data.go
@@ -5,36 +5,17 @@ import (
 	"strings"
 )
 
-// Data holds all file rows.
-type Data []Row
-
-// NewData creates new data.
-func NewData() Data {
-	return Data{}
-}
-
-func (d *Data) Add(in []byte) error {
-	r, err := NewRow(in)
-	if err != nil {
-		return fmt.Errorf("%q: %v", in, err)
-	}
-
-	*d = append(*d, r)
-
-	return nil
-}
-
 // String returns string representation.
-func (d *Data) String() string {
-	size := len(*d)
+func (d Data) String() string {
+	size := len(d)
 	if size < 1 {
 		return "empty data"
 	}
 
 	all := make([]string, 0, size)
 
-	for _, r := range *d {
-		s := fmt.Sprintf("%v", &r)
+	for _, r := range d {
+		s := fmt.Sprintf("%s", r)
 		all = append(all, s)
 	}
 
--- fileload/row.go
+++ fileload/row.go
@@ -1,43 +1,11 @@
 package fileload
 
 import (
-	"bytes"
 	"fmt"
-	"strconv"
 )
 
-// Row holds file row's data.
-type Row struct {
-	ID   int
-	Name []byte
-}
-
-// NewRow creates new Row.
-func NewRow(in []byte) (Row, error) {
-	sep := []byte{':'}
-
-	all := bytes.Split(in, sep)
-
-	const fields = 2
-	if len(all) < fields {
-		return Row{}, fmt.Errorf("fields: len < %v", fields)
-	}
-
-	id, err := strconv.Atoi(string(all[0]))
-	if err != nil {
-		return Row{}, fmt.Errorf("id: %v", err)
-	}
-
-	r := Row{
-		ID:   id,
-		Name: all[1],
-	}
-
-	return r, nil
-}
-
 // String returns string representation.
-func (r *Row) String() string {
+func (r Row) String() string {
 	s := fmt.Sprintf("id: %02d name: %q", r.ID, r.Name)
 	return s
 }
