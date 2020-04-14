e293ebb065b68e978b7280298164aa86dd076f5e
Davor Kapsa <davor.kapsa@gmail.com>
2020-04-14 14:30:11 +0200

    fileload/a: clean

--- fileload/a/main_test.go
+++ fileload/a/main_test.go
@@ -12,6 +12,7 @@ func TestParse(t *testing.T) {
 
 func BenchmarkParse(b *testing.B) {
 	var data fileload.Data
+
 	b.Run("99", func(b *testing.B) {
 		data = fileload.BenchParse(b, parse, fileload.Rows99)
 		_ = len(data)
