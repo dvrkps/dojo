6e09e13555e7d8e5f0b4adad0ba574607907d1c4
Davor Kapsa <davor.kapsa@gmail.com>
2020-06-04 18:07:57 +0200

    pills/in*/pharma: add All

--- pills/internal/pharma/main.go
+++ pills/internal/pharma/main.go
@@ -2,16 +2,69 @@
 package pharma
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"io"
+	"strings"
 	"time"
 
 	"github.com/dvrkps/dojo/pills/medicament"
 )
 
-type Pharma struct {
+// All holds pills.
+type All struct {
 	pills []medicament.Medicament
 }
 
-func New(t time.Time, r io.Reader) (*Pharma, error) {
-	return nil, nil
+// NewAll parses pills from file.
+func NewAll(r io.Reader, t time.Time) (*All, error) {
+	var all All
+	err := all.scan(r, t)
+	if err != nil {
+		return nil, err
+	}
+	return &all, nil
+}
+
+// String returns all pills string representation.
+func (a *All) String() string {
+	s := make([]string, len(a.pills))
+
+	for i := range a.pills {
+		s[i] = a.pills[i].String()
+	}
+
+	const newline = "\n"
+
+	return strings.Join(s, newline) + newline
+}
+
+func (a *All) scan(r io.Reader, t time.Time) error {
+	s := bufio.NewScanner(r)
+
+	const commentPrefix = "//"
+
+	for s.Scan() {
+		if bytes.HasPrefix(s.Bytes(), []byte(commentPrefix)) {
+			continue
+		}
+
+		if err := a.add(s.Bytes(), t); err != nil {
+			return err
+		}
+	}
+
+	return s.Err()
+}
+
+func (a *All) add(in []byte, t time.Time) error {
+	m, err := medicament.New(t, in)
+	if err != nil {
+		return fmt.Errorf("add: %v", err)
+	}
+
+	a.pills = append(a.pills, m)
+
+	return nil
 }
--- pills/internal/pharma/main_test.go
+++ pills/internal/pharma/main_test.go
@@ -1,1 +1,31 @@
 package pharma
+
+import (
+	"io"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestNewAll(t *testing.T) {
+	today := time.Date(2020, 6, 8, 1, 2, 3, 4, time.UTC)
+	all, err := NewAll(fakeFileContent(), today)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	got := len(all.pills)
+	const want = 3
+	if got != want {
+		t.Errorf("len got %v; want %v", got, want)
+	}
+}
+
+func fakeFileContent() io.Reader {
+	const newline = "\n"
+	const c = "2020-06-01,20,Dd,1\n" +
+		"//2020-06-01,31,Cc,1\n" +
+		"2020-06-01,19,Bb,1\n" +
+		"2020-06-01,18,Aa,1\n"
+
+	return strings.NewReader(c)
+}
