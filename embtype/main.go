87f6062559e3fdf9697c5dcd54eaa0d8cf980919
Davor Kapsa <davor.kapsa@gmail.com>
2020-06-13 18:26:44 +0200

    embtype: add namedPoint

--- embtype/main.go
+++ embtype/main.go
@@ -1,1 +1,27 @@
 package embtype
+
+import "fmt"
+
+type point struct {
+	X int
+	Y int
+}
+
+type namedPoint struct {
+	point
+	Name string
+}
+
+func newNamedPoint(name string, x, y int) namedPoint {
+	return namedPoint{
+		point: point{
+			X: x,
+			Y: y,
+		},
+		Name: name,
+	}
+}
+
+func (p namedPoint) String() string {
+	return fmt.Sprintf("%s x:%d y:%d", p.Name, p.X, p.Y)
+}
--- embtype/main_test.go
+++ embtype/main_test.go
@@ -1,1 +1,13 @@
 package embtype
+
+import (
+	"testing"
+)
+
+func Test(t *testing.T) {
+	got := newNamedPoint("one", 1, 2)
+	want := "one x:1 y:2"
+	if got.String() != want {
+		t.Errorf("got %q; want %q", got, want)
+	}
+}
