47250b3cdd5a2a3a7863c4d11afff376a4d29399
Davor Kapsa <davor.kapsa@gmail.com>
2020-06-25 13:11:25 +0200

    errors/isas: add myError

--- errors/isas/main.go
+++ errors/isas/main.go
@@ -1,1 +1,24 @@
 package isas
+
+import (
+	"errors"
+	"fmt"
+)
+
+var sentinelError = errors.New("sentinel error")
+
+type myError struct {
+	err error
+}
+
+func (e *myError) Error() string {
+	if e.err != nil {
+		return fmt.Sprintf("my: %v", e.err)
+	}
+
+	return "e: error"
+}
+
+func (e *myError) Unwrap() error {
+	return e.err
+}
--- errors/isas/main_test.go
+++ errors/isas/main_test.go
@@ -1,1 +1,27 @@
 package isas
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+func TestIsSentinelError(t *testing.T) {
+	fn := func() error {
+		return &myError{err: &myError{err: sentinelError}}
+	}
+
+	err := fn()
+
+	if !errors.Is(err, sentinelError) {
+		t.Fatalf("not sentinel error")
+	}
+
+	got := fmt.Sprintf("%v", err)
+
+	const want = "my: my: sentinel error"
+
+	if got != want {
+		t.Fatalf("got %q; want %q", got, want)
+	}
+}
