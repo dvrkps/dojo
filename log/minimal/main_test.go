32136613839e5b94d93e9f7e7f6d4db9d4255b0b
Davor Kapsa <davor.kapsa@gmail.com>
2020-07-10 10:44:04 +0200

    log/minimal: init tests

--- log/minimal/main_test.go
+++ log/minimal/main_test.go
@@ -1,1 +1,46 @@
 package minimal
+
+import "testing"
+
+func TestLog(t *testing.T) {
+	tests := []struct {
+		name    string
+		verbose bool
+		action  func(log *Log)
+		want    string
+	}{
+		{
+			name:    "F",
+			verbose: false,
+			action: func(log *Log) {
+				log.F("F %v", 42)
+			},
+			want: "F 42\n",
+		},
+		{
+			name:    "Vf verbose",
+			verbose: true,
+			action: func(log *Log) {
+				log.Vf("Vf verbose %v", 42)
+			},
+			want: "Vf verbose 42\n",
+		},
+		{
+			name:    "Vf",
+			verbose: false,
+			action: func(log *Log) {
+				log.Debugf("Vf %v", 42)
+			},
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		log, buf := testLogSetup(tt.verbose)
+		tt.action(log)
+		got := buf.String()
+		if got != tt.want {
+			t.Errorf("%s: = %q; want %q",
+				tt.name, got, tt.want)
+		}
+	}
+}
