9df567b55b574c0a5b4a537f37519b989a213c29
Davor Kapsa <davor.kapsa@gmail.com>
2020-07-10 10:53:28 +0200

    log/minimal: add New

--- log/minimal/main.go
+++ log/minimal/main.go
@@ -1,1 +1,21 @@
 package minimal
+
+import (
+	"io"
+	"log"
+)
+
+// A Log represents an logger.
+type Log struct {
+	output  *log.Logger
+	verbose bool
+}
+
+// New creates logger.
+func New(w io.Writer, prefix string) Log {
+	l := Log{}
+	if w != nil {
+		l.output = log.New(w, prefix, 0)
+	}
+	return l
+}
