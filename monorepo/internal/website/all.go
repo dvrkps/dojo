f0cad232f620e1ffe09f16959f75af5fe15c7cbb
Davor Kapsa <davor.kapsa@gmail.com>
2020-08-19 11:26:02 +0200

    monorepo/i: add website subfolders

---
+++ monorepo/internal/website/all.go
@@ -0,0 +1,26 @@
+package website
+
+import (
+	"fmt"
+
+	"github.com/dvrkps/dojo/monorepo"
+	"github.com/dvrkps/dojo/monorepo/internal/website/europe/bbccom"
+	"github.com/dvrkps/dojo/monorepo/internal/website/usa/timecom"
+)
+
+func All() (map[string]*monorepo.Website, error) {
+	ws := []*monorepo.Website{
+		bbccom.New(),
+		timecom.New(),
+	}
+	all := make(map[string]*monorepo.Website, len(ws))
+	for _, w := range all {
+		if _, ok := all[w.Key]; ok {
+			return nil, fmt.Errorf("duplicate key: %q", w.Key)
+		}
+
+		all[w.Key] = w
+	}
+
+	return all, nil
+}
---
+++ monorepo/internal/website/usa/timecom/main.go
@@ -0,0 +1,24 @@
+package timecom
+
+import (
+	"fmt"
+
+	"github.com/dvrkps/dojo/monorepo"
+)
+
+// New creates website.
+func New() *monorepo.Website {
+	return &monorepo.Website{
+		Key:   "bbc_com",
+		Index: index,
+		Parse: parse,
+	}
+}
+
+func index(w *monorepo.Website) (string, error) {
+	return fmt.Sprintf("%v: index", w.Key), nil
+}
+
+func parse(w *monorepo.Website) (string, error) {
+	return fmt.Sprintf("%v: parse", w.Key), nil
+}
