8166c86726f7dd15f93ad92b2489c5e9abb0c9ec
Davor Kapsa <davor.kapsa@gmail.com>
2020-08-19 11:26:45 +0200

    monorepo: rm target

--- monorepo/internal/target/europe/bbccom/main.go
+++
@@ -1,9 +0,0 @@
-package bbccom
-
-import "github.com/dvrkps/dojo/monorepo/internal/target"
-
-func New() target.Target {
-	return target.Target{
-		Key: "bbc_com",
-	}
-}
--- monorepo/internal/target/main.go
+++
@@ -1,6 +0,0 @@
-package target
-
-// Target holds target data.
-type Target struct {
-	Key string
-}
