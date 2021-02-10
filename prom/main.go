d189054eff5bc5c9d9b9c2380aa920902e8c90f9
Davor Kapsa <davor.kapsa@gmail.com>
2021-02-10 17:42:53 +0100

    prom: add restart metric

--- prom/main.go
+++ prom/main.go
@@ -11,19 +11,18 @@ import (
 )
 
 func main() {
-	dojoPings, err := startCounterVec("dojo_pings_total", "Number of pings", []string{"instance"})
+	dojoRestarts, err := startCounterVec("dojo_restarts_total", "Number of restarts", []string{"instance"})
 	if err != nil {
 		log.Printf("start: %v", err)
 		return
 	}
 
-	go startInstance("one", dojoPings)
+	go startInstance("one", dojoRestarts)
 
-	go startInstance("two", dojoPings)
+	go startInstance("two", dojoRestarts)
 
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
 
 func startCounterVec(name, help string, labels []string) (*prometheus.CounterVec, error) {
@@ -43,16 +42,28 @@ func startCounterVec(name, help string, labels []string) (*prometheus.CounterVec
 	return cv, nil
 }
 
-func startInstance(name string, c *prometheus.CounterVec) {
-	p := c.WithLabelValues(name)
+func startInstance(name string, restarts *prometheus.CounterVec) {
 	for {
-		p.Inc()
-		randomSleep()
+		done := make(chan struct{})
+		t := time.NewTimer(1 * time.Minute)
+
+		go func() {
+			<-t.C
+			close(done)
+		}()
+
+		const maxSleep = 1000000000
+		restarts.WithLabelValues(name).Inc()
+		for {
+			rs := randomNumber(maxSleep)
+			time.Sleep(time.Duration(rs))
+		}
+
+		println("restart " + name)
 	}
 }
 
-func randomSleep() {
+func randomNumber(max int) int {
 	rand.Seed(time.Now().UnixNano())
-	n := rand.Intn(1000000000)
-	time.Sleep(time.Duration(n))
+	return rand.Intn(max)
 }
