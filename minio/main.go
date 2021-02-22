468540b422d230fbdc123f63e3b3dbf20aa4b7b0
Davor Kapsa <davor.kapsa@gmail.com>
2021-02-22 12:44:49 +0100

    minio: add newMinioClient

--- minio/main.go
+++ minio/main.go
@@ -1,4 +1,63 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+const (
+	baseAddress = "localhost:9001"
+	tinyAddress = "localhost:9002"
+)
+
 func main() {
+	base, err := newMinioClient(baseAddress)
+	if err != nil {
+		log.Printf("base: %v", err)
+		return
+	}
+
+	tiny, err := newMinioClient(tinyAddress)
+	if err != nil {
+		log.Printf("tiny: %v", err)
+		return
+	}
+
+	_ = base
+	_ = tiny
+}
+
+const (
+	minioAccess = "accessKey"
+	minioSecret = "secretKey"
+)
+
+func newMinioClient(address string) (*minio.Client, error) {
+	c, err := minio.New(address, &minio.Options{
+		Creds:  credentials.NewStaticV4(minioAccess, minioSecret, ""),
+		Secure: false,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	const bucketName = "mybucket"
+
+	found, err := c.BucketExists(context.Background(), bucketName)
+	if err != nil {
+		return nil, fmt.Errorf("bucket: %v", err)
+	}
+
+	if !found {
+		err = c.MakeBucket(context.Background(), bucketName, minio.MakeBucketOptions{})
+		if err != nil {
+			return nil, fmt.Errorf("bucket make: %v", err)
+		}
+	}
+
+	return c, nil
 }
