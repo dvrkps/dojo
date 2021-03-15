0651cf75c9df616398e60bc965be5e3b564d8e05
Davor Kapsa <davor.kapsa@gmail.com>
2021-03-15 12:31:41 +0100

    clickhouse: connect to db

--- clickhouse/main.go
+++ clickhouse/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/ClickHouse/clickhouse-go"
+)
 
 func main() {
-	fmt.Println("clickhouse")
+	const dsn = "tcp://127.0.0.1:9000?" +
+		// "database=dojodb&" +
+		"password=dojopassword&" +
+		"debug=true"
+
+	connect, err := sql.Open("clickhouse", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := connect.Ping(); err != nil {
+		if exception, ok := err.(*clickhouse.Exception); ok {
+			fmt.Printf("[%d] %s \n%s\n", exception.Code, exception.Message, exception.StackTrace)
+		} else {
+			fmt.Println(err)
+		}
+		return
+	}
+
 }
