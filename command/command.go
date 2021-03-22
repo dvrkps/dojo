ceefd5a97a184c6af0fad7808daabc1190d475d9
Davor Kapsa <davor.kapsa@gmail.com>
2021-03-22 11:48:33 +0100

    command: add command

---
+++ command/command.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+func newCommand(args []string) (*Command, error) {
+	const argsError = `use "build", "run" or "test".`
+	if len(args) < 2 {
+		return nil, errors.New(argsError)
+	}
+
+	var (
+		c   Command
+		err error
+	)
+	switch args[1] {
+	case "one":
+		err = newOneCommand(&c)
+	case "two":
+		err = newTwoCommand(&c)
+	case "three":
+		err = newThreeCommand(&c)
+	default:
+		err = errors.New(argsError)
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("command: %v", err)
+	}
+
+	return &c, nil
+}
+
+type Command struct {
+	Kind int
+}
+
+const (
+	None = iota
+	OneCommand
+	TwoCommand
+	ThreeCommand
+)
+
+func newOneCommand(c *Command) error {
+	log.SetPrefix("one: ")
+
+	if c == nil {
+		return errors.New("nil command")
+	}
+
+	c.Kind = OneCommand
+
+	return nil
+}
+
+func newTwoCommand(c *Command) error {
+	log.SetPrefix("two: ")
+
+	if c == nil {
+		return errors.New("nil command")
+	}
+
+	c.Kind = TwoCommand
+
+	return nil
+}
+
+func newThreeCommand(c *Command) error {
+	log.SetPrefix("three: ")
+
+	if c == nil {
+		return errors.New("nil command")
+	}
+
+	c.Kind = ThreeCommand
+
+	return nil
+}
--- command/main.go
+++ command/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("command")
+	log.SetFlags(0)
+	log.SetPrefix("command: ")
+
+	log.Println("setup")
+
+	c, err := newCommand(os.Args)
+
+	if err != nil {
+		log.Printf("new command: %v", err)
+
+		return
+	}
+
+	if c.Kind == OneCommand {
+		log.Println("k1")
+	}
+
+	if c.Kind == TwoCommand {
+		log.Println("k2")
+	}
+
+	if c.Kind == ThreeCommand {
+		log.Println("k3")
+	}
+
+	log.Println("done")
 }
