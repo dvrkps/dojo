531f23ef2793e6628041c03df78b586dc71fd34e
Davor Kapsa <davor.kapsa@gmail.com>
2021-04-02 13:07:49 +0200

    testing/short: add sum func

--- testing/short/main.go
+++ testing/short/main.go
@@ -1,4 +1,5 @@
-package main
+package short
 
-func main() {
+func sum(a, b int) int {
+	return a + b
 }
--- testing/short/main_test.go
+++ testing/short/main_test.go
@@ -1,1 +1,11 @@
-package main
+package short
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	got := sum(1, 2)
+	const want = 3
+	if got != want {
+		t.Errorf("sum(1,2) = %v; want %v", got, want)
+	}
+}
