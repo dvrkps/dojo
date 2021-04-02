b26aae1fd59187089b9589aa15655d56e1495960
Davor Kapsa <davor.kapsa@gmail.com>
2021-04-02 13:10:38 +0200

    testing/short: add integration test

--- testing/short/main_test.go
+++ testing/short/main_test.go
@@ -9,3 +9,14 @@ func TestSum(t *testing.T) {
 		t.Errorf("sum(1,2) = %v; want %v", got, want)
 	}
 }
+
+func TestIntegration(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skip integration test")
+	}
+	got := sum(1, 2)
+	const want = 3
+	if got != want {
+		t.Errorf("sum(1,2) = %v; want %v", got, want)
+	}
+}
