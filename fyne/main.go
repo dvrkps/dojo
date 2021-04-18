c299ac7ae06b16616d30435ec6f3bf9de61dd9ff
Davor Kapsa <davor.kapsa@gmail.com>
2021-04-18 11:09:26 +0200

    fyne: window resize

--- fyne/main.go
+++ fyne/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -10,11 +13,17 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("fyne")
 
+	w.Resize(fyne.NewSize(300, 300))
+
 	hello := widget.NewLabel("Hello Fyne!")
+
 	w.SetContent(container.NewVBox(
 		hello,
 		widget.NewButton("Hi!", func() {
 			hello.SetText("Welcome :)")
+			w.Hide()
+			time.Sleep(5 * time.Second)
+			w.Show()
 		}),
 		widget.NewButton("Quit", func() {
 			a.Quit()
