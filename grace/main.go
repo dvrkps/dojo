a6b521656ae8a33aea1100e733d924cd4ebb13b1
Davor Kapsa <davor.kapsa@gmail.com>
2021-06-06 13:50:26 +0200

    grace: delete runCounter

--- grace/main.go
+++ grace/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"time"
 )
 
 func main() {
@@ -19,24 +18,4 @@ func main() {
 
 type Number struct {
 	Value int
-}
-
-func runCounter(ctx context.Context, out chan<- Number) error {
-	x := 0
-
-	for {
-		select {
-		case <-ctx.Done():
-			return fmt.Errorf("stoped at %v, err: %v", x, ctx.Err())
-		default:
-		}
-
-		x++
-
-		out <- Number{Value: x}
-
-		time.Sleep(1e9)
-	}
-
-	return nil
 }
