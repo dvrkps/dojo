a9bc30f42033e50a71de025c64c614ec9f27aa8e
Davor Kapsa <davor.kapsa@gmail.com>
2021-07-02 21:01:11 +0200

    clickhouse: fix fake client

--- clickhouse/database/fake/client.go
+++ clickhouse/database/fake/client.go
@@ -3,12 +3,12 @@ package fake
 import (
 	"context"
 
-	"github.com/dvrkps/dojo/clickhouse/database"
+	"github.com/dvrkps/dojo/clickhouse/clickhouse"
 )
 
 type Client struct {
 	Ping              func(context.Context) error
 	Close             func() error
 	CreateIfNotExists func(context.Context) error
-	InsertRow         func(context.Context, database.Row) error
+	InsertRow         func(context.Context, clickhouse.Row) error
 }
