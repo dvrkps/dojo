78a5a17eb514d03aafd6673e2cc78294301eea40
Davor Kapsa <davor.kapsa@gmail.com>
2021-07-05 16:55:55 +0200

    timecache: add TC and New

--- timecache/main.go
+++ timecache/main.go
@@ -1,1 +1,15 @@
 package timecache
+
+import "time"
+
+type TC struct {
+	delayDuration time.Duration
+}
+
+func New(delayDuration time.Duration) *TC {
+	tc := TC{
+		delayDuration: delayDuration,
+	}
+
+	return &tc
+}
--- timecache/main_test.go
+++ timecache/main_test.go
@@ -1,1 +1,14 @@
 package timecache
+
+import (
+	"testing"
+	"time"
+)
+
+func TestNew(t *testing.T) {
+	const delayDuration = 1 * time.Second
+	got := New(delayDuration)
+	if got == nil {
+		t.Error("nil TC")
+	}
+}
