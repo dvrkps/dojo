f043c24f0b349c3a572270223bdc9c6d370e1f02
Davor Kapsa <davor.kapsa@gmail.com>
2021-07-27 20:15:36 +0200

    clickhouse/fake: clean

--- clickhouse/real/fake/client.go
+++ clickhouse/real/fake/client.go
@@ -2,13 +2,42 @@ package fake
 
 import (
 	"context"
+	"errors"
 
-	"github.com/dvrkps/dojo/clickhouse/clickhouse"
+	"github.com/dvrkps/dojo/clickhouse/real"
 )
 
 type Client struct {
-	Ping              func(context.Context) error
-	Close             func() error
-	CreateIfNotExists func(context.Context) error
-	InsertRow         func(context.Context, clickhouse.Row) error
+	PingFunc              func(context.Context) error
+	CloseFunc             func() error
+	CreateIfNotExistsFunc func(context.Context) error
+	InsertRowFunc         func(context.Context, real.Row) error
+}
+
+func (c *Client) Close() error {
+	if c.CloseFunc == nil {
+		return errors.New("nil close func")
+	}
+	return c.CloseFunc()
+}
+
+func (c *Client) Ping(ctx context.Context) error {
+	if c.PingFunc == nil {
+		return errors.New("nil ping func")
+	}
+	return c.PingFunc(ctx)
+}
+
+func (c *Client) CreateIfNotExists(ctx context.Context) error {
+	if c.CreateIfNotExistsFunc == nil {
+		return errors.New("nil createIfNotExists func")
+	}
+	return c.CreateIfNotExistsFunc(ctx)
+}
+
+func (c *Client) InsertRow(ctx context.Context, r real.Row) error {
+	if c.CreateIfNotExistsFunc == nil {
+		return errors.New("nil createIfNotExists func")
+	}
+	return c.InsertRowFunc(ctx, r)
 }
