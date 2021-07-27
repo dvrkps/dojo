dc086f900ef7dd29a9cad4f1c2e2c7f600b63259
Davor Kapsa <davor.kapsa@gmail.com>
2021-07-27 10:14:03 +0200

    clickhouse/database: add client functions

--- clickhouse/database/client.go
+++ clickhouse/database/client.go
@@ -12,13 +12,47 @@ type RealClient interface {
 	CreateIfNotExists(ctx context.Context) error
 }
 
+var nilRealClientError = errors.New("nil real client")
+
 type Client struct {
 	realClient RealClient
 }
 
 func NewClient(rc RealClient) (*Client, error) {
 	if rc == nil {
-		return nil, errors.New("nil real client")
+		return nil, nilRealClientError
 	}
 	return &Client{realClient: rc}, nil
+}
+
+func (c *Client) Close() error {
+	if c.realClient == nil {
+		return nilRealClientError
+	}
+
+	return c.realClient.Close()
+}
+
+func (c *Client) Ping(ctx context.Context) error {
+	if c.realClient == nil {
+		return nilRealClientError
+	}
+
+	return c.realClient.Ping(ctx)
+}
+
+func (c *Client) InsertRow(ctx context.Context, r Row) error {
+	if c.realClient == nil {
+		return nilRealClientError
+	}
+
+	return c.realClient.InsertRow(ctx, r)
+}
+
+func (c *Client) CreateIfNotExists(ctx context.Context) error {
+	if c.realClient == nil {
+		return nilRealClientError
+	}
+
+	return c.realClient.CreateIfNotExists(ctx)
 }
