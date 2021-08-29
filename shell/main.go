1cbb42c532abcd9e0db2d787e100913b53d92345
Davor Kapsa <davor.kapsa@gmail.com>
2021-08-29 09:14:19 +0200

    shell: add run

--- shell/main.go
+++ shell/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	lgr := log.New(os.Stderr, "", 0)
+
+	err := run(os.Stdin, os.Stdout, os.Stderr)
+	if err != nil {
+		lgr.Printf("run: %v", err)
+		os.Exit(1)
+	}
+}
+
+func run(stdin io.Reader, stdout io.Writer, stderr io.Writer) error {
+	return nil
+}
