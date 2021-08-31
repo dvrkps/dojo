a73bee473bc23cdb66d158372693979d9ab65189
Davor Kapsa <davor.kapsa@gmail.com>
2021-08-31 17:23:15 +0200

    shell: clean run

--- shell/main.go
+++ shell/main.go
@@ -1,21 +1,42 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 	lgr := log.New(os.Stderr, "", 0)
 
-	err := run(os.Stdin, os.Stdout, os.Stderr)
+	err := run(os.Stdin, os.Stdout)
 	if err != nil {
 		lgr.Printf("run: %v", err)
 		os.Exit(1)
 	}
 }
 
-func run(stdin io.Reader, stdout io.Writer, stderr io.Writer) error {
+func run(stdin io.Reader, stdout io.Writer) error {
+	reader := bufio.NewReader(stdin)
+	for {
+		// Read the keyboad input.
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			return err
+		}
+		cmd := strings.TrimSpace(line)
+		if cmd == "quit" {
+			break
+		}
+
+		_, err = fmt.Fprintln(stdout, cmd)
+		if err != nil {
+			return err
+		}
+
+	}
 	return nil
 }
