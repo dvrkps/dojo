08780fdd9b9283166054e89034aa6f52d504a245
Davor Kapsa <davor.kapsa@gmail.com>
2021-09-06 17:53:07 +0200

    shell: add signal notify

--- shell/main.go
+++ shell/main.go
@@ -2,46 +2,57 @@ package main
 
 import (
 	"bufio"
+	"context"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"os/signal"
 	"strings"
 )
 
 func main() {
 	lgr := log.New(os.Stderr, "", 0)
 
-	err := run(os.Stdin, os.Stdout)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	err := run(ctx, os.Stdin, os.Stdout)
 	if err != nil {
-		lgr.Printf("run: %v", err)
-		os.Exit(1)
+		if !errors.Is(err, context.Canceled) {
+			lgr.Printf("run: %v", err)
+			os.Exit(1)
+		}
 	}
 }
 
-func run(stdin io.Reader, stdout io.Writer) error {
+func run(ctx context.Context, stdin io.Reader, stdout io.Writer) error {
 	r := bufio.NewReader(stdin)
 	for {
-		_, err := fmt.Fprintf(stdout, "> ")
-		if err != nil {
-			return err
-		}
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			_, err := fmt.Fprintf(stdout, "> ")
+			if err != nil {
+				return err
+			}
 
-		raw, err := r.ReadString('\n')
-		if err != nil {
-			return err
-		}
+			raw, err := r.ReadString('\n')
+			if err != nil {
+				return err
+			}
 
-		cmd := strings.TrimSpace(raw)
-		if cmd == "exit" {
-			break
-		}
+			cmd := strings.TrimSpace(raw)
+			if cmd == "exit" {
+				return nil
+			}
 
-		_, err = fmt.Fprintln(stdout, cmd)
-		if err != nil {
-			return fmt.Errorf("write: %v", err)
+			_, err = fmt.Fprintln(stdout, cmd)
+			if err != nil {
+				return fmt.Errorf("write: %v", err)
+			}
 		}
 	}
-
-	return nil
 }
