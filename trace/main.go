9702659398cf547379d7fea164a0efc023f38f81
Davor Kapsa <davor.kapsa@gmail.com>
2021-09-12 08:55:22 +0200

    trace: setup trace file

--- trace/main.go
+++ trace/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+	"runtime/trace"
+)
+
+func main() {
+	lgr := log.New(os.Stderr, "", 0)
+
+	f, err := os.Create("trace.out")
+	if err != nil {
+		lgr.Printf("create: %v", err)
+	}
+	defer func() {
+		errClose := f.Close()
+		if errClose != nil {
+			lgr.Printf("close: %v", errClose)
+		}
+	}()
+
+	err = trace.Start(f)
+	if err != nil {
+		lgr.Printf("start: %v", err)
+	}
+	defer trace.Stop()
+}
