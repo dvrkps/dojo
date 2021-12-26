2118af176d9f65140d70b3b8cd57fc89d99b10de
Davor Kapsa <davor.kapsa@gmail.com>
2021-12-26 11:20:31 +0100

    repo/cmd/repo: clean main

--- repo/cmd/repo/main.go
+++ repo/cmd/repo/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"github.com/dvrkps/dojo/repo/internal/mysql"
+)
+
 func main() {
-	println("eto")
+	const repoDSN = "root:example@tcp(127.0.0.1:4406)/repodb"
+
+	db, err := mysql.ConnectDB(context.Background(), repoDSN)
+	if err != nil {
+		log.Printf("connect: %v", err)
+	}
+
+	defer func() {
+		err = db.Close()
+		if err != nil {
+			log.Printf("close: %v", err)
+		}
+	}()
+
+	println("done")
 }
