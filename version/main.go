a4f1def5b0fe8fa32ba3a9918e8f9f8091b4e49b
Davor Kapsa <davor.kapsa@gmail.com>
2021-12-26 13:44:30 +0100

    version: get version from build info data(v1.18beta1)

--- version/main.go
+++ version/main.go
@@ -1,3 +1,34 @@
 package main
 
-func main() {}
+import (
+	"runtime/debug"
+)
+
+func main() {
+	println(version())
+}
+
+func version() string {
+	const v = "v0.1.0"
+
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return v
+	}
+
+	s := bi.Settings
+
+	var commit = ""
+	for i := range s {
+		if s[i].Key == "vcs.revision" {
+			commit = s[i].Value
+			break
+		}
+	}
+
+	if commit == "" {
+		return v
+	}
+
+	return v + "-" + commit
+}
