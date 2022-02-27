6a251c4806b04747a14f77d739a28cc0a307b6f9
Davor Kapsa <davor.kapsa@gmail.com>
2022-02-27 19:24:46 +0100

    ddd: add real

---
+++ ddd/real/users.go
@@ -0,0 +1,17 @@
+package real
+
+import (
+	"context"
+
+	"github.com/dvrkps/dojo/ddd/user"
+)
+
+type Service struct{}
+
+func (s *Service) Users(ctx context.Context) ([]user.User, error) {
+	r := []user.User{
+		{ID: 1, Name: "Real one"},
+		{ID: 2, Name: "Real two"},
+	}
+	return r, nil
+}
