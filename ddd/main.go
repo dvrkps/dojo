9d417ff1c8f5dce6b91a5bf1a5841c92b21fd7fd
Davor Kapsa <davor.kapsa@gmail.com>
2022-02-27 19:30:47 +0100

    ddd: user real.Service

--- ddd/main.go
+++ ddd/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/dvrkps/dojo/ddd/real"
+)
+
+func main() {
+	s := real.Service{}
+
+	us, err := s.Users(context.Background())
+	if err != nil {
+		log.Printf("users: %v", err)
+		return
+	}
+
+	for _, u := range us {
+		fmt.Println(u.ID, u.Name)
+	}
+}
