80481a4d85119a4117c176b427e357a0bbf2637b
Davor Kapsa <davor.kapsa@gmail.com>
2022-02-27 20:25:38 +0100

    ddd: user mock in tests

--- ddd/main_test.go
+++ ddd/main_test.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"context"
+	"testing"
+
+	"github.com/dvrkps/dojo/ddd/real/mock"
+	"github.com/dvrkps/dojo/ddd/user"
+)
+
+func TestRun(t *testing.T) {
+	s := mock.Service{
+		UsersFn: func(ctx context.Context) ([]user.User, error) {
+			return mock.Users(), nil
+		},
+	}
+
+	got, err := run(context.Background(), &s)
+	if err != nil {
+		t.Errorf("err: %v", err)
+	}
+
+	want := mock.Users()
+	if !compare(got, want) {
+		t.Error("got not equal want")
+	}
+}
+
+func compare(got, want []user.User) bool {
+	if len(got) != len(want) {
+		return false
+	}
+	for i := range got {
+		if got[i] != want[i] {
+			return false
+		}
+	}
+	return true
+}
