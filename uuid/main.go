39b070b0704541ed5543ad92423570e65c204434
Davor Kapsa <davor.kapsa@gmail.com>
2022-09-08 18:45:16 +0200

    uuid: add google/uuid

--- uuid/main.go
+++ uuid/main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"log"
 
+	google "github.com/google/uuid"
 	satori "github.com/satori/go.uuid"
 )
 
 func main() {
 	s, err := satori.NewV4()
 	if err != nil {
-		log.Println(err)
+		log.Println("s:", err)
 	}
-	println(s.String())
+	println("s: ", s.String())
+
+	g, err := google.NewRandom()
+	if err != nil {
+		log.Println("g:", err)
+	}
+
+	println("g: ", g.String())
 }
