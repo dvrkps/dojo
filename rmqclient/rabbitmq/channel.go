8d552041b3035586e00dc5d1b626a666d01c4cf6
Davor Kapsa <davor.kapsa@gmail.com>
2022-10-01 23:15:03 +0200

    rmqclient/rabbitmq: add ConsumeParams

--- rmqclient/rabbitmq/channel.go
+++ rmqclient/rabbitmq/channel.go
@@ -8,7 +8,7 @@ import (
 
 type Channel interface {
 	Close() error
-	Consume(queue, consumer string, autoAck, exclusive, noLocal, noWait bool, args Table) (<-chan Delivery, error)
+	Consume(p *ConsumeParams) (<-chan Delivery, error)
 	PublishWithContext(ctx context.Context, exchange, key string, mandatory, immediate bool, msg Publishing) error
 	QueueDeclare(name string, durable, autoDelete, exclusive, noWait bool, args Table) (Queue, error)
 }
@@ -21,8 +21,18 @@ func (c *channel) Close() error {
 	return c.channel.Close()
 }
 
-func (c *channel) Consume(queue, consumer string, autoAck, exclusive, noLocal, noWait bool, args Table) (<-chan Delivery, error) {
-	return c.channel.Consume(queue, consumer, autoAck, exclusive, noLocal, noWait, args)
+type ConsumeParams struct {
+	Queue     string
+	Consumer  string
+	AutoAck   bool
+	Exclusive bool
+	NoLocal   bool
+	NoWait    bool
+	Args      Table
+}
+
+func (c *channel) Consume(p *ConsumeParams) (<-chan Delivery, error) {
+	return c.channel.Consume(p.Queue, p.Consumer, p.AutoAck, p.Exclusive, p.NoLocal, p.NoWait, p.Args)
 }
 
 func (c *channel) PublishWithContext(ctx context.Context, exchange, key string, mandatory, immediate bool, msg Publishing) error {
